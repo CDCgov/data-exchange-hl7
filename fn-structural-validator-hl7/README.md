5594857983fafc24b8d40fb35b827ef209f35c41
cosmin <lrp4@cdc.gov>
2022-07-13 20:45:38 -0400

    fn hl7 with wired in sample

---
+++ fn-structural-validator-hl7/README.md
@@ -0,0 +1,15 @@
+
+
+### Install hl7-structural-validator
+
+```scala
+mvn install:install-file \
+    -Dfile=artifacts/cdc.xlr.hl7.structurevalidator-assembly-0.3.1.jar \
+    -DgroupId=cdc.xlr.structurevalidator \
+    -DartifactId=cdc-xlr-hl7-structurevalidator \
+    -Dversion=0.3.1 \
+    -Dpackaging=jar \
+    -DgeneratePom=true
+```
+
+
--- fn-structural-validator-hl7/src/main/java/com/function/hl7/structure/validator/Function.scala
+++ fn-structural-validator-hl7/src/main/java/com/function/hl7/structure/validator/Function.scala
@@ -11,6 +11,9 @@ import com.microsoft.azure.functions.annotation.HttpTrigger;
 
 import java.util.Optional;
 
+import cdc.xlr.structurevalidator._;
+
+
 /**
  * Azure Functions with HTTP Trigger.
  */
@@ -28,6 +31,18 @@ class Function {
           authLevel = AuthorizationLevel.ANONYMOUS) request: HttpRequestMessage[Optional[String]],
         context: ExecutionContext): HttpResponseMessage = {
         context.getLogger.info("Scala HTTP trigger processed a request.")
-        request.createResponseBuilder(HttpStatus.OK).body("fn Scala").build
+        
+        // Genv2_2-0-1_TC01 from https://ndc.services.cdc.gov/mmgpage/generic-v2-0-message-mapping-guide/
+        val testMsg = """MSH|^~\&|SendAppName^2.16.840.1.114222.TBD^ISO|Sending-Facility^2.16.840.1.114222.TBD^ISO|PHINCDS^2.16.840.1.114222.4.3.2.10^ISO|PHIN^2.16.840.1.114222^ISO|20140630120030.1234-0500||ORU^R01^ORU_R01|TM_CN_TC01_GENV2|T|2.5.1|||||||||NOTF_ORU_v3.0^PHINProfileID^2.16.840.1.114222.4.10.3^ISO~Generic_MMG_V2.0^PHINMsgMapID^2.16.840.1.114222.4.10.4^ISO
+PID|1||GenV2_TC01^^^SendAppName&2.16.840.1.114222.nnnn&ISO||~^^^^^^S||19640502|F||2106-3^White^CDCREC|^^^48^77018^^^^48201|||||||||||2135-2^Hispanic or Latino^CDCREC|||||||20140302
+OBR|1||GenV2_TC01^SendAppName^2.16.840.1.114222.nnnn^ISO|68991-9^Epidemiologic Information^LN|||20140227170100|||||||||||||||20140227170100|||F||||||11550^Hemolytic uremic syndrome postdiarrheal^NND
+OBX|1|ST|32624-9^Other Race Text^LN||Apache||||||F
+OBX|2|CWE|78746-5^Country of Birth^LN||USA^UNITED STATES OF AMERICA^ISO3166_1||||||F
+OBX|3|CWE|77983-5^Country of Usual Residence^LN||USA^UNITED STATES OF AMERICA^ISO3166_1||||||F
+OBX|4||11368-8^Date of Illness Onset^LN||20140224||||||F"""
+        val validator = StructureValidatorSync()
+        val result = validator.validate(testMsg)
+
+        request.createResponseBuilder(HttpStatus.OK).body(result).build
     } // .HttpTrigger
 }
