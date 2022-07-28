4614822ae8173c7c41f4f61ffec8e3af3faf5f5d
cosmin <lrp4@cdc.gov>
2022-07-28 09:56:45 -0400

    hl7 str val - added one more test case

--- hl7-structural-validator/src/test/scala/StructureValidatorTest.scala
+++ hl7-structural-validator/src/test/scala/StructureValidatorTest.scala
@@ -34,6 +34,31 @@ class StructureValidatorTest extends AnyFlatSpec with should.Matchers {
 
     } // .in
 
+    "Test message: PERT_V1.0.1_TM_TC04" should "have 6 structure errors, no content and no value set errors with sync validator" in {
+
+        // load test file from resources
+        val fileName = "PERT_V1.0.1_TM_TC04"
+        val hl7TestMessageLoc = HL7_TEST_MESSAGES_LOCATION + fileName + ".txt"
+        val hl7TestMessage = Source.fromFile(hl7TestMessageLoc).getLines.mkString("\n")
+
+        val validator = StructureValidatorSync()
+
+        validator.reportMap(hl7TestMessage) match {
+
+            case Success(report) => {
+                report("contentErrors").size shouldBe(0) 
+                report("valueSetErrors").size shouldBe(0) 
+                report("structureErrors").size shouldBe(6) 
+            } // .Success
+            case Failure(e) => {
+                println("error: " + e.getMessage() )
+                fail("validate test message should not throw exception")
+            } // .Failure 
+
+        } // .match
+
+    } // .in
+
     "Test message empty or without MSH segment" should "throw with message: No MSH Segment found in the message." in {
 
         val validator = StructureValidatorConc()
