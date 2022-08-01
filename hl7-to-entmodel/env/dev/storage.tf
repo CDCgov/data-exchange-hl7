6118ccc0e23f43cd6c258a8ebf399f7d4e080bbe
cosmin <cosmycx@gmail.com>
2022-08-01 16:02:10 -0400

    hl7-to-entmodel terraform added storage

---
+++ hl7-to-entmodel/env/dev/storage.tf
@@ -0,0 +1,15 @@
+
+resource "azurerm_storage_account" "sa" {
+
+  name                     = "${var.storage_prefix_function}_${var.environment_name}"
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_kind             = "StorageV2"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = var.environment_name
+  }
+
+}
