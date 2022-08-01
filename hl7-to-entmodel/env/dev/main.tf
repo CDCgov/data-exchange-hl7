8b42adddfd1de39836ac4420f35e0371ebe4b90b
cosmin <cosmycx@gmail.com>
2022-08-01 14:43:20 -0400

    hl7-to-entmodel started terraform

---
+++ hl7-to-entmodel/env/dev/main.tf
@@ -0,0 +1,84 @@
+#############################################################################
+# TERRAFORM CONFIG
+#############################################################################
+
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.16.0"
+    }
+  }
+}
+
+#############################################################################
+# VARIABLES
+#############################################################################
+
+variable "resource_group_name" {
+  type = string
+  default = "TODO"
+}
+
+variable "location" {
+  type    = string
+  default = "eastus"
+}
+
+
+variable "vnet_cidr_range" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "subnet_prefixes" {
+  type    = list(string)
+  default = ["10.0.0.0/24", "10.0.1.0/24"]
+}
+
+variable "subnet_names" {
+  type    = list(string)
+  default = ["pipeline_hl7", "dbx"]
+}
+
+#############################################################################
+# PROVIDERS
+#############################################################################
+
+provider "azurerm" {
+  features {}
+}
+
+#############################################################################
+# RESOURCES
+#############################################################################
+
+resource "azurerm_resource_group" "vnet_main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+module "vnet-main" {
+  source              = "Azure/vnet/azurerm"
+  version             = "~> 2.6.0"
+  resource_group_name = azurerm_resource_group.vnet_main.name
+  vnet_name           = var.resource_group_name
+  address_space       = [var.vnet_cidr_range]
+  subnet_prefixes     = var.subnet_prefixes
+  subnet_names        = var.subnet_names
+  nsg_ids             = {}
+
+  tags = {
+    environment = "dev"
+  }
+
+  depends_on = [azurerm_resource_group.vnet_main]
+}
+
+#############################################################################
+# OUTPUTS
+#############################################################################
+
+output "vnet_id" {
+  value = module.vnet-main.vnet_id
+}
