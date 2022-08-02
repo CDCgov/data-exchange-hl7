bda3b0bfbe9ab9d8d95e535dece59426a714e683
cosmin <cosmycx@gmail.com>
2022-08-02 08:46:20 -0400

    hl7-to-entmodel tf scripts

--- hl7-to-entmodel/terraform/outputs.tf
+++ hl7-to-entmodel/terraform/outputs.tf
@@ -8,6 +8,26 @@ output "vnet_id" {
   description = "Deployed vnet id"
 }
 
+output "storage_fn" {
+  value = azurerm_storage_account.storage_fn.name
+  description = "Deployed storage name"
+}
+
+output "app_service_plan" {
+  value = azurerm_service_plan.app_service_plan.name
+  description = "Deployed App Service name"
+}
+
+output "eventhub_namespace" {
+  value = azurerm_eventhub_namespace.eventhub_namespace.name
+  description = "Deployed Event Hubs namespace name"
+}
+
+output "eventhub" {
+  value = azurerm_eventhub.eventhub.name
+  description = "Deployed Event Hubs name"
+}
+
 output "fn_sv_app_name" {
   value = azurerm_linux_function_app.fn_app_sv.name
   description = "Deployed function app structural validator name"
