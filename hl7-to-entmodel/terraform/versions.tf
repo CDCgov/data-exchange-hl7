ed1d137f424f918ad63f5e08ea9e8df9c2ce9d63
cosmin <cosmycx@gmail.com>
2022-08-01 21:17:43 -0400

    hl7-to-entmodel tf changes

--- hl7-to-entmodel/terraform/versions.tf
+++ hl7-to-entmodel/terraform/versions.tf
@@ -1,7 +1,13 @@
+
 #############################################################################
-# PROVIDERS
+# TERRAFORM VERSIONS
 #############################################################################
 
-provider "azurerm" {
-  features {}
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.16.0"
+    }
+  }
 }
