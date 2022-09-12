bd04ebe19b825c7e7ad1bee1021e9604d3450140
Neelima Kandukuri <dtx0@cdc.gov>
2022-09-12 08:25:33 -0400

    Added TimeTrigger scheduler & deleted HttpTrigger

--- phin-vocab/src/main/java/cdc/gov/phinvads/vocab/TimerTriggerFunction.kt
+++ phin-vocab/src/main/java/cdc/gov/phinvads/vocab/TimerTriggerFunction.kt
@@ -2,42 +2,33 @@ package cdc.gov.phinvads.vocab
 
 import com.google.gson.GsonBuilder
 import com.microsoft.azure.functions.*
-import com.microsoft.azure.functions.annotation.AuthorizationLevel
 import com.microsoft.azure.functions.annotation.FunctionName
-import com.microsoft.azure.functions.annotation.HttpTrigger
+import com.microsoft.azure.functions.annotation.TimerTrigger
 import gov.cdc.vocab.service.bean.ValueSet
 import gov.cdc.vocab.service.bean.ValueSetConcept
 import redis.clients.jedis.DefaultJedisClientConfig
 import redis.clients.jedis.Jedis
 import java.util.*
-import kotlin.concurrent.thread
+
 
 /**
- * Azure Functions with HTTP Trigger.
+ * Azure Functions with Timer trigger.
  */
-class HttpTriggerFunction {
+class TimerTriggerFunction {
     /**
-     * This function listens at endpoint "/api/HttpExample". Two ways to invoke it using "curl" command in bash:
-     * 1. curl -d "HTTP Body" {your host}/api/HttpExample
-     * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
+     * This function will be invoked periodically according to the specified schedule.
      */
     @FunctionName("VocabReadFunction")
     fun run(
-        @HttpTrigger(
-            name = "req",
-            methods = [HttpMethod.GET, HttpMethod.POST],
-            authLevel = AuthorizationLevel.ANONYMOUS
-        ) request: HttpRequestMessage<Optional<String?>?>,
+        @TimerTrigger(name = "timerInfo", schedule = "0 30 16 * * FRI") timerInfo: String?,
         context: ExecutionContext
-    ): HttpResponseMessage {
-        try {
-           thread {
+   ) {
                context.logger.info("Java HTTP trigger processed a request.")
-               var redisCacheName = System.getenv("REDISCACHEHOSTNAME")
-               var rediscachekey = System.getenv("REDISCACHEKEY")
+               val redisCacheName = System.getenv("REDISCACHEHOSTNAME")
+               val rediscachekey = System.getenv("REDISCACHEKEY")
                // redisCacheName = "temedehl7.redis.cache.windows.net"
                // rediscachekey ="1ofYheE07YlzGOuDAauSQwQ09tj5u4fVrAzCaKAsQt0="
-               println("cacheHostname :${redisCacheName} ")
+                context.logger.info("cacheHostname :${redisCacheName} ")
 
                // Connect to the Azure Cache for Redis over the TLS/SSL port using the key.
                val jedis = Jedis(
@@ -47,13 +38,17 @@ class HttpTriggerFunction {
                        .build()
                )
                // Simple PING command
-               println( "\nCache Command  : Ping" )
-               println( "Cache Response : " + jedis.ping())
+               context.logger.info( "\nCache Command  : Ping" )
+               context.logger.info( "Cache Response : " + jedis.ping())
                //println( "Cache Response : " + jedis.set("Message", "Test"))
 
                val client = VocabClient()
-               println("STARTING VocabClient services")
+               context.logger.info("STARTING VocabClient services")
                val valueSets = client.getAllValueSets() as List<ValueSet>?
+
+              if (valueSets != null) {
+                  context.logger.info("Count of ValueSets:  ${valueSets.size}")
+              }
                val vocabMap: MutableMap<StringBuilder, Any> = HashMap()
                val gson = GsonBuilder().create()
                for (e in valueSets!!) {
@@ -64,15 +59,8 @@ class HttpTriggerFunction {
                        jedis.del(key.toString())
                    jedis.set(key.toString(), gson.toJson(valueSetConcepts))
                }
-               println("END OF VocabClient services")
+               context.logger.info("END OF VocabClient services")
                jedis.close()
 
-           }.start()
-        } catch (re:Exception) {
-           println("exception in thread:  $re");
-        }
-
-        return request.createResponseBuilder(HttpStatus.OK).body("phinvads function execution started in the background").build()
-
     }
-}
+    }
