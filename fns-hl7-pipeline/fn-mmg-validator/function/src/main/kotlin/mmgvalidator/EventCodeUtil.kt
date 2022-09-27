68e65fb224eed2464c91877e6abd6145fcfd9eb7
lrp4 <lrp4@cdc.gov>
2022-09-27 09:04:03 -0400

    fns-pipeline event code util added comment

--- fns-hl7-pipeline/fn-mmg-validator/function/src/main/kotlin/mmgvalidator/EventCodeUtil.kt
+++ fns-hl7-pipeline/fn-mmg-validator/function/src/main/kotlin/mmgvalidator/EventCodeUtil.kt
@@ -7,12 +7,12 @@ class EventCodeUtil () {
   fun getMmgName(eventCode: String): String {
 
     return when( eventCode ) {
-      "11089" -> "LYME"
-      "11080" -> "TBRD" 
-      "11088" -> "TBRD"
-      "11090" -> "TBRD"
-      "11091" -> "TBRD"
-      "10250" -> "TBRD"
+      "11089" -> "LYME" // | MMG UUID
+      "11080" -> "TBRD" // | MMG UUID
+      "11088" -> "TBRD" // | MMG UUID
+      "11090" -> "TBRD" // | MMG UUID
+      "11091" -> "TBRD" // | MMG UUID
+      "10250" -> "TBRD" // | MMG UUID
       else -> HL7Error.EVENT_CODE_NOT_SUPPORTED_ERROR.message
     } // .when
 
