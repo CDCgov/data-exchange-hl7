7d7ae2fa2e6adc72a7eac91883b7f7588d594a49
Marcelo Caldas <mscaldas@gmail.com>
2022-10-27 12:53:20 -0400

    Receiver metadata (#240)
    * refactoring receiver-debatcher for proper metadata
    
    * refactoring receiver-debatcher for proper metadata
    
    * fixed error posting message to event hub
    
    * fn-receiver-debatcher naming event hub send params
    
    * fn-receiver-debatcher logs change
    
    Co-authored-by: Caldas <MCQ1@cdc.gov>
    Co-authored-by: lrp4 <lrp4@cdc.gov>

---
+++ fns-hl7-pipeline/fn-receiver-debatcher/src/main/kotlin/gov/cdc/dex/hl7/receiver/ReceiverProcessMetadata.kt
@@ -0,0 +1,10 @@
+package gov.cdc.dex.hl7.receiver
+
+import gov.cdc.dex.metadata.ProcessMetadata
+
+data class ReceiverProcessMetadata (override val status: String?): ProcessMetadata(RECEIVER_PROCESS, RECEIVER_VERSION , status) {
+    companion object  {
+        const val RECEIVER_PROCESS = "RECEIVER"
+        const val RECEIVER_VERSION = "1.0.0"
+    }
+}
