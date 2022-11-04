f32badda5dd1204ecebbf85709f86710709776dd
Caldas <MCQ1@cdc.gov>
2022-11-04 11:25:33 -0400

    removing duplicate underscores after string normalization

--- local_libs/lib-dex-commons/src/main/kotlin/gov/cdc/dex/util/StringUtils.kt
+++ local_libs/lib-dex-commons/src/main/kotlin/gov/cdc/dex/util/StringUtils.kt
@@ -4,12 +4,12 @@ class StringUtils {
     companion object {
         fun normalizeString(str: String): String {
             val replaceableChars = listOf(" ", "-", "/", "&")
-
             var rr1 = str.trim().lowercase()
             replaceableChars.forEach {
                 rr1 = rr1.replace(it, "_")
             }
-            return rr1
+            //remove duplicate underscores based on replacements above.
+            return rr1.replace("(_)\\1+".toRegex(), "_" )
         }
     }
 }
