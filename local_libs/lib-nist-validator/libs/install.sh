291f6dca9226601892c9300b345ae4347a00cadf
Caldas <MCQ1@cdc.gov>
2022-12-19 13:54:19 -0500

    adding POM files

--- local_libs/lib-nist-validator/libs/install.sh
+++ local_libs/lib-nist-validator/libs/install.sh
@@ -1,7 +1,9 @@
 
-mvn deploy:deploy-file -DrepositoryId=nexus -Durl=https://imagehub.cdc.gov/repository/maven-ede/ -Dfile=hl7-v2-parser-1.5.1.jar -DgroupId=gov.nist -DartifactId=hl7-v2-parser -Dversion=1.5.1 -Dpackaging=jar
+mvn deploy:deploy-file -DrepositoryId=nexus -Durl=https://imagehub.cdc.gov/repository/maven-ede/ -Dfile=hl7-v2-profile-1.5.1.jar -DgroupId=gov.nist -DartifactId=hl7-v2-profile -Dversion=1.5.1 -Dpackaging=jar -DpomFile=hl7-v2-profile-1.5.1.pom
 
-mvn deploy:deploy-file -DrepositoryId=nexus -Durl=https://imagehub.cdc.gov/repository/maven-ede/ -Dfile=hl7-v2-profile-1.5.1.jar -DgroupId=gov.nist -DartifactId=hl7-v2-profile -Dversion=1.5.1 -Dpackaging=jar
+mvn deploy:deploy-file -DrepositoryId=nexus -Durl=https://imagehub.cdc.gov/repository/maven-ede/ -Dfile=hl7-v2-parser-1.5.1.jar -DgroupId=gov.nist -DartifactId=hl7-v2-parser -Dversion=1.5.1 -Dpackaging=jar -DpomFile=hl7-v2-parser-1.5.1.pom
 
-mvn deploy:deploy-file -DrepositoryId=nexus -Durl=https://imagehub.cdc.gov/repository/maven-ede/ -Dfile=hl7-v2-validation-1.5.1.jar -DgroupId=gov.nist -DartifactId=hl7-v2-validation -Dversion=1.5.1 -Dpackaging=jar
 
+mvn deploy:deploy-file -DrepositoryId=nexus -Durl=https://imagehub.cdc.gov/repository/maven-ede/ -Dfile=hl7-v2-validation-1.5.1.jar -DgroupId=gov.nist -DartifactId=hl7-v2-validation -Dversion=1.5.1 -Dpackaging=jar -DpomFile=hl7-v2-validation-1.5.1.pom
+
+mvn deploy:deploy-file -DrepositoryId=nexus -Durl=https://imagehub.cdc.gov/repository/maven-ede/ -Dfile= -DgroupId=gov.nist -DartifactId=xml-util -Dversion=1.0.1-SNAPSHOT -Dpackaging=jar
