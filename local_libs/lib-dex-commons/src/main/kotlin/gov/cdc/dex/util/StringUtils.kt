f7ae1be0aeeb13e0ce791e8d43228c6f8e45c329
Caldas <MCQ1@cdc.gov>
2023-02-10 13:41:54 -0500

    allowing to call normalize() for null strings

--- local_libs/lib-dex-commons/src/main/kotlin/gov/cdc/dex/util/StringUtils.kt
+++ local_libs/lib-dex-commons/src/main/kotlin/gov/cdc/dex/util/StringUtils.kt
@@ -1,11 +1,27 @@
 package gov.cdc.dex.util
 
+import gov.cdc.dex.util.StringUtils.Companion.toHex
 import java.security.MessageDigest
 import kotlin.text.Charsets.UTF_8
 
 class StringUtils {
+
     companion object {
-        fun normalizeString(str: String): String {
+        //Public Functions:
+        fun String.hashMD5(): String {
+            return md5(this).toHex()
+        }
+
+        @JvmName("normalizeNullable")
+        fun String?.normalize(): String? {
+            return this?.normalize()
+        }
+        fun String.normalize(): String {
+            return normalizeString(this)
+        }
+
+
+        private fun normalizeString(str: String): String {
             val replaceableChars = mapOf(
                 " " to "_",
                 "-" to "_",
@@ -21,13 +37,11 @@ class StringUtils {
             return rr1.replace("(_)\\1+".toRegex(), "_" ).replace("[^A-Z a-z 0-9 _\\.]".toRegex(), "")
         }
 
-        fun String.normalize(): String {
-            return normalizeString(this)
-        }
+
         private fun md5(str: String): ByteArray = MessageDigest.getInstance("MD5").digest(str.toByteArray(UTF_8))
         private fun ByteArray.toHex() = joinToString(separator = "") { byte -> "%02x".format(byte) }
-        fun String.hashMD5(): String {
-            return md5(this).toHex()
-        }
+
+
     }
+
 }
