7a3cb99c14c4b626dc13bab3a9df0c8818559011
lrp4 <lrp4@cdc.gov>
2023-02-14 10:31:43 -0500

    fn-lake segs integrate hl7 pet tree and test

---
+++ fns-hl7-pipeline/fn-lake-segs-transfomer/src/main/kotlin/gov/cdc/dex/hl7/model/Profile.kt
@@ -0,0 +1,19 @@
+// package gov.cdc.dex.hl7.model 
+
+// // import gov.cdc.hl7.model.Profile
+
+// data class Profile ( 
+
+//   val segmentDefinition: Map<String, ProfileEntry>, 
+
+// ) // .Profile
+
+
+// data class ProfileEntry(
+
+//     val cardinality: String,
+//     val children: Map<String, ProfileEntry>?,
+
+// ) // .ProfileEntry
+
+
--- fns-hl7-pipeline/fn-lake-segs-transfomer/src/test/kotlin/LakeSegsTransTest.kt
+++ fns-hl7-pipeline/fn-lake-segs-transfomer/src/test/kotlin/LakeSegsTransTest.kt
@@ -7,23 +7,94 @@ import kotlin.test.assertEquals
 import org.slf4j.LoggerFactory
 import gov.cdc.dex.hl7.TransformerSegments
 
+// No longer used
+// import com.google.gson.Gson
+// import com.google.gson.GsonBuilder
+// import com.google.gson.reflect.TypeToken
+
+import gov.cdc.hl7.HL7HierarchyParser
+import gov.cdc.hl7.model.HL7Hierarchy 
 
 class LakeSegsTransTest {
 
     companion object {
 
         val logger = LoggerFactory.getLogger(LakeSegsTransTest::class.java.simpleName)
-
     } // .companion 
 
 
     @Test
-    fun testNothing() {
-        logger.info("testing log ok")
-    } // .testRedisInstanceUsed
+    fun testLoggerInfo() {
+
+        logger.info("testLoggerInfo...")
+    } // .testLoggerInfo
+
+    // No longer used
+    // @Test
+    // fun testLoadBasicProfileWithGson() {
+
+    //     val profileFilePath = "/BasicProfile.json"
+    //     val profileMapJson = this::class.java.getResource(profileFilePath).readText()
+    //     val profileMapType = object : TypeToken< Profile >() {}.type
+
+    //     val basicProfile: Profile = Gson().fromJson(profileMapJson, profileMapType)
+    
+    //     logger.info("basicProfile: --> $basicProfile")
+    // } // .testLoadBasicProfileWithGson
+
+
+    @Test
+    fun testReadLocalResources() {
+
+        logger.info("testReadLocalResources...")
+
+        // read the profile
+        val profileFilePath = "/BasicProfile.json"
+        val profile = this::class.java.getResource(profileFilePath).readText()
+
+        assertEquals(profile.substring(0, 100).length, 100)
+
+
+        // read the message:
+        val messagePath = "/Genv2_2-0-1_TC01.hl7"
+        val message = this::class.java.getResource(messagePath).readText()
+
+        assertEquals(message.substring(0, 100).length, 100)
+    } // .testParseMessageHierarchyFromJson
+
+
+    @Test
+    fun testHierarchyParser() {  
+
+        logger.info("testHierarchyParser...")
+        
+        fun printTree(node: HL7Hierarchy, ident: String) {   
+            println("$ident --> ${node.segment()}")  
+             node.children().foreach {   
+                printTree(it, ident + " ")
+            }
+        } // .printTree
+
+
+        // read the profile
+        val profileFilePath = "/BasicProfile.json"
+        val profile = this::class.java.getResource(profileFilePath).readText()
+
+        // read the message:
+        val messagePath = "/Genv2_2-0-1_TC01.hl7"
+        val message = this::class.java.getResource(messagePath).readText()
+
+        val msgTree = HL7HierarchyParser.parseMessageHierarchyFromJson(message, profile)
+        printTree(msgTree, "")
+    } // .testHierarchyParser
+
+
 
     @Test
     fun testTransformerSegments() {
+
+        logger.info("testTransformerSegments...")
+
         
         val filePath = "/TBRD_V1.0.2_TM_TC04.hl7"
         val testMsg = this::class.java.getResource(filePath).readText()
@@ -33,12 +104,12 @@ class LakeSegsTransTest {
 
         val lakeSegsModel = TransformerSegments().hl7ToSegments(testMsg)
 
-        // logger.info(lakeSegsModel.toString())
+        logger.info(lakeSegsModel.toString().substring(0, 10))
 
     } // .testTransformerSegments
 
 
-} // .MmgSqlTest
+} // .MmgSLakeSegsTransTestqlTest
 
 
 
