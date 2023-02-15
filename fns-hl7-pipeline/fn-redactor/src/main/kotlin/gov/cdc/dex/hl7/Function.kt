c492ecd727c6ec0f4fcd3c18af94fd8c1eebcebe
Neelima Kandukuri <dtx0@cdc.gov>
2023-02-15 10:15:03 -0500

    updates

--- fns-hl7-pipeline/fn-redactor/src/main/kotlin/gov/cdc/dex/hl7/Function.kt
+++ fns-hl7-pipeline/fn-redactor/src/main/kotlin/gov/cdc/dex/hl7/Function.kt
@@ -11,6 +11,7 @@ import gov.cdc.dex.hl7.model.RedactorProcessMetadata
 import gov.cdc.dex.metadata.Problem
 import gov.cdc.dex.metadata.SummaryInfo
 import gov.cdc.dex.util.DateHelper.toIsoString
+import gov.cdc.dex.util.JsonHelper
 import gov.cdc.dex.util.JsonHelper.addArrayElement
 import gov.cdc.dex.util.JsonHelper.gson
 import gov.cdc.dex.util.JsonHelper.toJsonElement
@@ -36,43 +37,43 @@ class Function {
     ) {
         context.logger.info("------ received event: ------> message: --> $message")
 
-        val REDACTOR_STATUS_OK = "PROCESS_REDACTOR_OK"
         val startTime = Date().toIsoString()
         val evHubNameOk = System.getenv("EventHubSendOkName")
         val evHubNameErrs = System.getenv("EventHubSendErrsName")
         val evHubConnStr = System.getenv("EventHubConnectionString")
 
         val ehSender = EventHubSender(evHubConnStr)
+        var hl7Content : String
+        var metadata : JsonObject
+        var filePath : String
+        var messageUUID : String
+        var helper = Helper()
 
         message.forEach { singleMessage: String? ->
             context.logger.info("------ singleMessage: ------>: --> $singleMessage")
             val inputEvent: JsonObject = JsonParser.parseString(singleMessage) as JsonObject
             try {
                 // Extract from event
-                var hl7ContentBase64 = inputEvent["content"].asString
-                val hl7ContentDecodedBytes = Base64.getDecoder().decode(hl7ContentBase64)
-                val hl7Content = String(hl7ContentDecodedBytes)
-                val metadata = inputEvent["metadata"].asJsonObject
-                val provenance = metadata["provenance"].asJsonObject
-                val filePath = provenance["file_path"].asString
-                val messageUUID = inputEvent["message_uuid"].asString
+                hl7Content = JsonHelper.getValueFromJsonAndBase64Decode("content", inputEvent)
+                metadata = JsonHelper.getValueFromJson("metadata", inputEvent).asJsonObject
+
+                filePath = JsonHelper.getValueFromJson("metadata.provenance.file_path", inputEvent).asString
+                messageUUID = JsonHelper.getValueFromJson("message_uuid", inputEvent).asString
+
 
                 context.logger.info("Received and Processing messageUUID: $messageUUID, filePath: $filePath")
 
-                var d = DeIdentifier()
-                var helper = Helper()
                 val report = helper.getRedactedReport(hl7Content)
-                val processMD = RedactorProcessMetadata(status = REDACTOR_STATUS_OK, report = report?._2()?.toList())
+                val processMD = RedactorProcessMetadata(status = "PROCESS_REDACTOR_OK", report = report?._2()?.toList())
                 processMD.startProcessTime = startTime
                 processMD.endProcessTime = Date().toIsoString()
 
                 metadata.addArrayElement("processes", processMD)
-
-                val newContentBase64 =
-                    Base64.getEncoder().encodeToString((report?._1()?.toByteArray() ?: "") as ByteArray?)
+                //println("new content: ${report?._1()}")
+                val newContentBase64 = Base64.getEncoder().encodeToString((report?._1()?.toByteArray() ?: "") as ByteArray?)
                 inputEvent.add("content", JsonParser.parseString(gson.toJson(newContentBase64)))
 
-                println("inputEvent new :${inputEvent}")
+                //println("inputEvent new :${inputEvent}")
                 context.logger.info("Handled Redaction for messageUUID: $messageUUID, filePath: $filePath, ehDestination: $evHubNameOk ")
                 context.logger.finest("INPUT EVENT OUT: --> ${gson.toJson(inputEvent)}")
                 ehSender.send(evHubNameOk, Gson().toJson(inputEvent))
