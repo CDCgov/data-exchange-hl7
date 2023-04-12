0761cbf1b3c0288524e669d8af97fb1d33ae3281
Marcia Schulman <quq6@cdc.gov>
2023-04-12 15:57:51 -0400

    add environment parameter

--- tools/upload-messages-py/upload_messages.py
+++ tools/upload-messages-py/upload_messages.py
@@ -10,47 +10,56 @@ x-tp-reporting_jurisdiction: (state FIPS number, only for ELR)
 x-tp-orginal_file_name:
 '''
 user_id = ""
-base_url = 'https://cloud-svc-transport-dev-eastus.azurewebsites.net/'
+env = "dev"
+ENVIRONMENTS = ["dev", "tst"]
 upload_url = "hl7ingress?filename="
 
+def upload_files(path_to_files, file_list):
+    base_url = f'https://cloud-svc-transport-{env}-eastus.azurewebsites.net/'
 
-def upload_file(path_to_file):
-    file_text = ""
-    original_name = os.path.basename(path_to_file)
-    # get plain text
-    with open(path_to_file, 'r', encoding='utf-8') as f:
-        file_text = f.read()
-    
-    if len(file_text) > 0:
-        # set parameters
-        header = {"x-tp-message_type": "CASE", "x-tp-orginal_file_name": original_name, "content-type": "text/plain"}
+    for file_name in file_list:
+        file_text = ""
+        full_path = os.path.join(path_to_files, file_name)
 
-        # upload the file
-        guid = uuid5(NAMESPACE_URL, path_to_file)
-        new_filename = f"upload-{user_id}-{guid}.txt"
-        full_url = f'{base_url}{upload_url}{new_filename}'
-        resp = requests.post(url=full_url, data=file_text, headers=header)
-        if resp.status_code == 200:
-            print(f"Success: file {path_to_file} --> {resp.text}")
-        else:
-            print(f'Problem uploading file {original_name}. Status code {resp.status_code}, message {resp.text}')
-    else:
-        print(f'Unable to upload {original_name} - no content found')
+        # get plain text
+        with open(full_path, 'r', encoding='utf-8') as f:
+            file_text = f.read()
         
+        if len(file_text) > 0:
+            # set header values
+            header = {"x-tp-message_type": "CASE", "x-tp-original_file_name": file_name, "content-type": "text/plain"}
+
+            # upload the file
+            guid = uuid5(NAMESPACE_URL, full_path)
+            new_filename = f"upload-{user_id}-{guid}.txt"
+
+            full_url = f'{base_url}{upload_url}{new_filename}'
+            resp = requests.post(url=full_url, data=file_text, headers=header)
+
+            if resp.status_code == 200:
+                print(f"Success: file {file_name} --> {resp.text}")
+            else:
+                print(f'Problem uploading file {file_name}. Status code {resp.status_code}, message {resp.text}')
+        else:
+            print(f'Unable to upload {file_name} - no content found')
+            
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python upload_messages.py <path to HL7 message files> <user_id>")
+    if len(sys.argv) != 4:
+        print("Usage: python upload_messages.py <path to HL7 message files> <user_id> <environment>")
     else:
         path = sys.argv[1]
         user_id = sys.argv[2]
-        if os.path.exists(path):
+        env = sys.argv[3].lower()
+
+        if (env not in ENVIRONMENTS):
+            print(f"Error: environment must be one of these values: {ENVIRONMENTS}")
+        elif os.path.exists(path):
             file_list = [s for s in os.listdir(path) if s.lower().endswith(".hl7") or s.lower().endswith(".txt")]
             if len(file_list) == 0:
                 print(f"No HL7 files found in {path}.")
             else:
-                for f in file_list:
-                    upload_file(os.path.join(path, f))
+                upload_files(path, file_list)
                 print("DONE -- Upload of folder completed.")
         else:
             print(f"Path {path} does not exist.")
