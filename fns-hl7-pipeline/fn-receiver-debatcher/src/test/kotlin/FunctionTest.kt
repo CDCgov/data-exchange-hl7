d3349179489ab221c318f73b9cc3b944443f7238
uil9 <uil9@cdc.gov>
2023-04-24 11:15:41 -0400

    #uil9_859 --> Jacoco code coverage

--- fns-hl7-pipeline/fn-receiver-debatcher/src/test/kotlin/FunctionTest.kt
+++ fns-hl7-pipeline/fn-receiver-debatcher/src/test/kotlin/FunctionTest.kt
@@ -1,7 +1,7 @@
 import com.microsoft.azure.functions.ExecutionContext
 import gov.cdc.dex.azure.EventHubMetadata
 import gov.cdc.dex.hl7.receiver.Function
-import org.junit.Test
+import org.junit.jupiter.api.Test
 import java.io.File
 import java.util.logging.Logger
 
@@ -9,18 +9,18 @@ class FunctionTest {
 
     @Test
     fun callReceiverDebatcherFunction_happyPath() {
+        println("Starting Function test")
         val function = Function()
         val text = File("src/test/resources/message.txt").readText()
         //JsonParser.parseString(text)
 
         val messages: MutableList<String> = ArrayList()
-        if (text != null) {
             messages.add(text)
             val eventHubMDList: MutableList<EventHubMetadata> = ArrayList()
             val eventHubMD = EventHubMetadata(1, 99, "", "")
             eventHubMDList.add(eventHubMD)
             function.eventHubProcessor(messages, eventHubMDList, getExecutionContext()!!)
-        }
+        println("Finished Function test")
     }
 
 
