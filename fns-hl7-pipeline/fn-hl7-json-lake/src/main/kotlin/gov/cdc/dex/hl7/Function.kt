49bcd0c38bcc8419f9942989386a2ba55b20ca75
Caldas <MCQ1@cdc.gov>
2023-06-06 17:08:17 -0400

    fixing bugs withing hl7-json fn

--- fns-hl7-pipeline/fn-hl7-json-lake/src/main/kotlin/gov/cdc/dex/hl7/Function.kt
+++ fns-hl7-pipeline/fn-hl7-json-lake/src/main/kotlin/gov/cdc/dex/hl7/Function.kt
@@ -13,6 +13,7 @@ import gov.cdc.dex.azure.EventHubSender
 import gov.cdc.dex.metadata.Problem
 import gov.cdc.dex.metadata.SummaryInfo
 import gov.cdc.dex.util.DateHelper.toIsoString
+import gov.cdc.dex.util.JsonHelper
 import gov.cdc.dex.util.JsonHelper.addArrayElement
 import gov.cdc.dex.util.JsonHelper.toJsonElement
 import gov.cdc.hl7.bumblebee.HL7JsonTransformer
@@ -31,6 +32,9 @@ class Function {
         val PROCESS_STATUS_OK = "SUCCESS"
         val PROCESS_STATUS_EXCEPTION = "FAILURE"
 
+        val gsonWithNullsOn: Gson = GsonBuilder().serializeNulls().create()
+
+        val fnConfig = FunctionConfig()
     } // .companion object
 
 
@@ -40,94 +44,85 @@ class Function {
             name = "msg",
             eventHubName = "%EventHubReceiveNameCASE%",
             connection = "EventHubConnectionString",
-            consumerGroup = "%EventHubConsumerCASEGroup%",)
-        message: List<String?>,
+            consumerGroup = "%EventHubConsumerGroupCASE%",)
+        messages: List<String?>,
         @BindingName("SystemPropertiesArray")eventHubMD:List<EventHubMetadata>,
         context: ExecutionContext) {
-
-        // context.logger.info("------ received event: ------> message: --> $message")
-
-        val startTime =  Date().toIsoString()
-
-        val gsonWithNullsOn: Gson = GsonBuilder().serializeNulls().create() //.setPrettyPrinting().create()
-
-        // Set up the 2 out Event Hubs: OK and Errs
-        val evHubConnStr = System.getenv("EventHubConnectionString")
-        val eventHubSendOkName = System.getenv("EventHubSendOkName")
-        val eventHubSendErrsName = System.getenv("EventHubSendErrsName")
-        val evHubSender = EventHubSender(evHubConnStr)
 
         //
         // Process each Event Hub Message
         // ----------------------------------------------
         // message.forEach { singleMessage: String? ->
-        message.forEachIndexed {
-                messageIndex: Int, singleMessage: String? ->
-            // context.logger.info("------ singleMessage: ------>: --> $singleMessage")
-            try {
+        processAllMessages(messages, eventHubMD, context) // .message.forEach
 
+    } // .eventHubProcessor
+    @FunctionName("HL7_JSON_LAKE_TRANSFORMER_ELR")
+    fun eventHubELRProcessor(
+        @EventHubTrigger(
+            name = "msg",
+            eventHubName = "%EventHubReceiveNameELR%",
+            connection = "EventHubConnectionString",
+            consumerGroup = "%EventHubConsumerGroupELR%",)
+        messages: List<String?>,
+        @BindingName("SystemPropertiesArray")eventHubMD:List<EventHubMetadata>,
+        context: ExecutionContext) {
+        //
+        // Process each Event Hub Message
+        // ----------------------------------------------
+        // message.forEach { singleMessage: String? ->
+        processAllMessages(messages, eventHubMD, context)
+
+    } // .eventHubProcessor
+
+    private fun processAllMessages( messages: List<String?>, eventHubMD: List<EventHubMetadata>, context: ExecutionContext) {
+        messages.forEachIndexed { messageIndex: Int, singleMessage: String? ->
+            val startTime = Date().toIsoString()
+            try {
                 val inputEvent: JsonObject = JsonParser.parseString(singleMessage) as JsonObject
-                // context.logger.info("------ inputEvent: ------>: --> $inputEvent")
-                val metadata = inputEvent["metadata"].asJsonObject
-                val provenance = metadata["provenance"].asJsonObject
-                val filePath = provenance["file_path"].asString
+                val metadata = JsonHelper.getValueFromJson("metadata", inputEvent).asJsonObject
+                val filePath =JsonHelper.getValueFromJson("metadata.provenance.file_path", inputEvent).asString
                 val messageUUID = inputEvent["message_uuid"].asString
 
-                // read the profile
-                val profileFilePath = "/PhinGuidProfile.json"
-                val profile = this::class.java.getResource(profileFilePath).readText()
                 try {
                     processMessage(
                         inputEvent,
-                        profile,
-                        eventHubMD,
-                        messageIndex,
-                        profileFilePath,
+                        eventHubMD[messageIndex],
                         startTime,
                         metadata,
-                        evHubSender,
-                        eventHubSendOkName,
-                        gsonWithNullsOn
+                        fnConfig.eventHubSendOkName
                     )
                 } catch (e: Exception) {
-
                     context.logger.severe("Exception: Unable to process Message messageUUID: $messageUUID, filePath: $filePath, due to exception: ${e.message}")
                     //publishing the message  to the eventhubSendErrsName topic using EventHub
-                    processMessageError(e, inputEvent, eventHubSendOkName, evHubSender, gsonWithNullsOn)
+                    processMessageError(e,inputEvent,fnConfig.eventHubSendOkName,fnConfig.evHubSender)
 
-                    context.logger.info("Processed for HL7 JSON Lake messageUUID: $messageUUID, filePath: $filePath, ehDestination: $eventHubSendErrsName")
+                    context.logger.info("Processed for HL7 JSON Lake messageUUID: $messageUUID, filePath: $filePath, ehDestination: ${fnConfig.eventHubSendErrsName}")
                 } // .catch
 
             } catch (e: Exception) {
-
                 // message is bad, can't extract fields based on schema expected
                 context.logger.severe("Unable to process Message due to exception: ${e.message}")
                 e.printStackTrace()
 
             } // .catch
 
-        } // .message.forEach
-
-    } // .eventHubProcessor
+        }
+    }
 
     private fun processMessage(
         inputEvent: JsonObject,
-        profile: String,
-        eventHubMD: List<EventHubMetadata>,
-        messageIndex: Int,
-        profileFilePath: String,
+        eventHubMD: EventHubMetadata,
         startTime: String,
         metadata: JsonObject,
-        evHubSender: EventHubSender,
-        eventHubSendOkName: String,
-        gsonWithNullsOn: Gson
+        eventHubSendOkName: String
     ) {
-        val bumblebee = HL7JsonTransformer.getTransformerWithResource(inputEvent.asString, profile)
+        val hl7message = JsonHelper.getValueFromJsonAndBase64Decode("content", inputEvent)
+        val bumblebee = HL7JsonTransformer.getTransformerWithResource(hl7message, fnConfig.profile)
         val fullHL7 = bumblebee.transformMessage()
 
         val processMD = HL7JSONLakeProcessMetadata(
-            status = PROCESS_STATUS_OK, eventHubMD = eventHubMD[messageIndex], report = fullHL7,
-            config = listOf(profileFilePath)
+            status = PROCESS_STATUS_OK, eventHubMD = eventHubMD, report = fullHL7,
+            config = listOf(FunctionConfig.PROFILE_FILE_PATH)
         )
 
         // process time
@@ -137,97 +132,15 @@ class Function {
         metadata.addArrayElement("processes", processMD)
 
         // enable for model
-
-        evHubSender.send(evHubTopicName = eventHubSendOkName, message = gsonWithNullsOn.toJson(inputEvent))
+        fnConfig.evHubSender.send(evHubTopicName = eventHubSendOkName, message = gsonWithNullsOn.toJson(inputEvent))
     }
 
 
-    @FunctionName("HL7_JSON_LAKE_TRANSFORMER_ELR")
-    fun eventHubELRProcessor(
-        @EventHubTrigger(
-            name = "msg",
-            eventHubName = "%EventHubReceiveNameELR%",
-            connection = "EventHubConnectionString",
-            consumerGroup = "%EventHubConsumerELRGroup%",)
-        message: List<String?>,
-        @BindingName("SystemPropertiesArray")eventHubMD:List<EventHubMetadata>,
-        context: ExecutionContext) {
 
-        // context.logger.info("------ received event: ------> message: --> $message")
-
-        val startTime =  Date().toIsoString()
-
-        val gsonWithNullsOn: Gson = GsonBuilder().serializeNulls().create() //.setPrettyPrinting().create()
-
-        // Set up the 2 out Event Hubs: OK and Errs
-        val evHubConnStr = System.getenv("EventHubConnectionString")
-        val eventHubSendELROkName = System.getenv("EventHubSendELROkName")
-        val eventHubSendErrsName = System.getenv("EventHubSendErrsName")
-        val evHubSender = EventHubSender(evHubConnStr)
-
-        //
-        // Process each Event Hub Message
-        // ----------------------------------------------
-        // message.forEach { singleMessage: String? ->
-        message.forEachIndexed {
-                messageIndex: Int, singleMessage: String? ->
-            // context.logger.info("------ singleMessage: ------>: --> $singleMessage")
-            try {
-
-                val inputEvent: JsonObject = JsonParser.parseString(singleMessage) as JsonObject
-                // context.logger.info("------ inputEvent: ------>: --> $inputEvent")
-                val metadata = inputEvent["metadata"].asJsonObject
-                val provenance = metadata["provenance"].asJsonObject
-                val filePath = provenance["file_path"].asString
-                val messageUUID = inputEvent["message_uuid"].asString
-
-                try {
-                    // read the profile
-                    val profileFilePath = "/PhinGuidProfile.json"
-                    val profile = this::class.java.getResource(profileFilePath).readText()
-                    processMessage(
-                        inputEvent,
-                        profile,
-                        eventHubMD,
-                        messageIndex,
-                        profileFilePath,
-                        startTime,
-                        metadata,
-                        evHubSender,
-                        eventHubSendELROkName,
-                        gsonWithNullsOn
-                    )
-                }
-                catch (e: Exception) {
-
-                    context.logger.severe("Exception: Unable to process Message messageUUID: $messageUUID, filePath: $filePath, due to exception: ${e.message}")
-                    //publishing the message  to the eventhubSendErrsName topic using EventHub
-                    processMessageError(e, inputEvent, eventHubSendErrsName, evHubSender, gsonWithNullsOn)
-
-                    context.logger.info("Processed for HL7 JSON Lake messageUUID: $messageUUID, filePath: $filePath, ehDestination: $eventHubSendErrsName")
-                } // .catch
-
-
-            } catch (e: Exception) {
-
-                // message is bad, can't extract fields based on schema expected
-                context.logger.severe("Unable to process Message due to exception: ${e.message}")
-                e.printStackTrace()
-
-            } // .catch
-
-        } // .message.forEach
-
-    } // .eventHubProcessor
 
 
     private fun processMessageError(
-        e: Exception,
-        inputEvent: JsonObject,
-        eventHubSendErrsName: String,
-        evHubSender: EventHubSender,
-        gsonWithNullsOn: Gson
-    ) {
+        e: Exception, inputEvent: JsonObject, eventHubSendErrsName: String, evHubSender: EventHubSender) {
         //TODO::  - update retry counts
         val problem = Problem(HL7JSONLakeProcessMetadata.PROCESS_NAME, e, false, 0, 0)
         val summary = SummaryInfo(PROCESS_STATUS_EXCEPTION, problem)
---
+++ fns-hl7-pipeline/fn-hl7-json-lake/src/main/kotlin/gov/cdc/dex/hl7/FunctionConfig.kt
@@ -0,0 +1,19 @@
+package gov.cdc.dex.hl7
+
+import gov.cdc.dex.azure.EventHubSender
+
+class FunctionConfig {
+    companion object {
+        val PROFILE_FILE_PATH = "/PhinGuidProfile.json"
+        val evHubConnStr = System.getenv("EventHubConnectionString")
+
+
+    }
+    val eventHubSendOkName = System.getenv("EventHubSendOkName")
+    val eventHubSendErrsName = System.getenv("EventHubSendErrsName")
+
+    val evHubSender = EventHubSender(evHubConnStr)
+    val profile = this::class.java.getResource(PROFILE_FILE_PATH).readText()
+
+
+}
