595fa803a79e9e31b264b63bf59cb87eab8e0347
Marcia Schulman <quq6@cdc.gov>
2023-06-14 16:19:29 -0400

    add logging and change to class structure

--- tools/upload-messages-py/upload_messages.py
+++ tools/upload-messages-py/upload_messages.py
@@ -10,54 +10,57 @@ x-tp-route:         (COVID19-ELR, PHLIP_FLU, PHLIP_VPD, only for ELR)
 x-tp-reporting_jurisdiction: (state FIPS number, only for ELR)
 x-tp-orginal_file_name: will be filled in by script
 '''
-user_id = ""
-env = "dev"
 ENVIRONMENTS = ["dev", "tst", "stg"]
 upload_url = "hl7ingress?filename="
-base_url = f'https://ocio-ede-{env}-hl7-svc-transport.azurewebsites.net/'
-path_to_files = ""
+upload_log = "./upload_log.txt"
 thread_local = threading.local()
 
-def get_session():
-    if not hasattr(thread_local, "session"):
-        thread_local.session = requests.Session()
-    return thread_local.session
+class FileUploader():
+    def __init__(self, env, path, user_id):
+        self.path_to_files = path
+        self.base_url = f'https://ocio-ede-{env}-hl7-svc-transport.azurewebsites.net/'
+        self.user_id = user_id
 
-def upload_all_files(path, file_list):
-    path_to_files = path
-    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
-        executor.map(upload_file, file_list)
+    def get_session(self):
+        if not hasattr(thread_local, "session"):
+            thread_local.session = requests.Session()
+        return thread_local.session
 
-def upload_file(file_name):
-        
-        file_text = ""
-        full_path = os.path.join(path_to_files, file_name)
-        norm_name = normalize(file_name)
+    def upload_all_files(self, file_list):      
+        with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+            executor.map(self.upload_file, file_list)
 
-        # get plain text
-        with open(full_path, 'r') as f:
-            file_text = f.read()
-        
-        if len(file_text) > 0:
-            # set header values
-            header = {"x-tp-message_type": "CASE", "x-tp-route": "PHLIP_VPD", "x-tp-reporting_jurisdiction": "06", "x-tp-original_file_name": file_name, "content-type": "text/plain"}
+    def upload_file(self, file_name):
+            
+            file_text = ""
+            full_path = os.path.join(self.path_to_files, file_name)
+            norm_name = self.normalize(file_name)
 
-            # upload the file
-            new_filename = f"upload-{user_id}-{norm_name}.txt"
-            full_url = f'{base_url}{upload_url}{new_filename}'
+            # get plain text
+            with open(full_path, 'r') as f:
+                file_text = f.read()
+            
+            with open(upload_log, '+a') as log:
+                if len(file_text) > 0:
+                    # set header values
+                    header = {"x-tp-message_type": "CASE", "x-tp-route": "PHLIP_VPD", "x-tp-reporting_jurisdiction": "06", "x-tp-original_file_name": file_name, "content-type": "text/plain"}
 
-            session = get_session()
-            with session.post(url=full_url, data=file_text, headers=header) as resp:
-                if resp.status_code == 200:
-                    print(f"Success: file {file_name} --> {resp.text}")
+                    # upload the file
+                    new_filename = f"upload-{self.user_id}-{norm_name}.txt"
+                    full_url = f'{self.base_url}{upload_url}{new_filename}'
+
+                    session = self.get_session()
+                    with session.post(url=full_url, data=file_text, headers=header) as resp:
+                        if resp.status_code == 200:
+                            log.write(f"Success: file {file_name} --> {resp.text}\n")
+                        else:
+                            log.write(f'Problem uploading file {file_name}. Status code {resp.status_code}, message {resp.text}\n')
                 else:
-                    print(f'Problem uploading file {file_name}. Status code {resp.status_code}, message {resp.text}')
-        else:
-            print(f'Unable to upload {file_name} - no content found')
+                    log.write(f'Unable to upload {file_name} - no content found\n')
 
-def normalize(name): 
-    return name.replace(".", "_").replace(" ", "_").replace("-", "_").replace("(", "").replace(")", "").replace("&", "and").lower()        
-          
+    def normalize(self, name): 
+        return name.replace(".", "_").replace(" ", "_").replace("-", "_").replace("(", "").replace(")", "").replace("&", "and").lower()        
+            
 
 if __name__ == "__main__":
     if len(sys.argv) != 4:
@@ -75,7 +78,8 @@ if __name__ == "__main__":
                 print(f"No HL7 files found in {path}.")
             else:
                 start_time = time.time()
-                upload_all_files(path, file_list)
+                uploader = FileUploader(env, path, user_id)
+                uploader.upload_all_files(file_list)
                 duration = time.time() - start_time
                 print("DONE -- Upload of folder completed.")
                 print(f"Uploaded {len(file_list)} files in {duration} seconds")
