97d7a089e6e18ed054fc44db6606dfb68b2f84dd
Caldas <MCQ1@cdc.gov>
2023-07-24 11:26:46 -0400

    cleaning up duplicate code on separate pacakges

--- local_libs/lib-bumblebee/src/test/kotlin/gov/cdc/hl7/bumblebee/TestTransformation.kt
+++ local_libs/lib-bumblebee/src/test/kotlin/gov/cdc/hl7/bumblebee/TestTransformation.kt
@@ -1,10 +1,9 @@
 package gov.cdc.hl7.bumblebee
 
 import org.junit.jupiter.api.Test
-import com.google.gson.JsonParser
-import gov.cdc.hl7.HL7StaticParser
+import gov.cdc.hl7.HL7ParseUtils
 
-//import gov.cdc.ncezid.bumblebee.TemplateTransformer
+//import gov.cdc.dex.TemplateTransformer
 class TestTransformation {
 
 
@@ -12,71 +11,74 @@ class TestTransformation {
     @Test
     fun testTransformation() {
         val bumblebee = TemplateTransformer.getTransformerWithResource("/labTemplate.json", "/BasicProfile.json")
-        val message = this::class.java.getResource("/RawHL7DataDAART.txt").readText()
+        val message = this::class.java.getResource("/RawHL7DataCELR.txt").readText()
+        val hl7Parser : HL7ParseUtils = HL7ParseUtils.getParser(message, "BasicProfile.json")
+        val obrs = hl7Parser.getValue("OBR").get().toList()
+        val epiOBRs = hl7Parser.getValue("OBR[@4.1='$EPI_OBR||NOTF||PERSUBJ']").get().toList()
+        val nonEpiOBRs = obrs.filter{ innerArray -> innerArray !in epiOBRs }
+        println("obrs: ${obrs.size}, epiOBRs: ${epiOBRs.size}, nonEpiOBRS: ${nonEpiOBRs.size}")
 
-        //val obrs = HL7StaticParser.getValue(message, "OBR")
-
-        val lines =message.split("\n")
-
-        var oneLab = ""
-        var foundOBR = false
-        lines.forEach {
-            if (it.startsWith("OBR|")) { //New Lab?
-                //have we collected a lab yet?
-                if (foundOBR) {
-                    val newMessage = bumblebee.transformMessage(oneLab)
-                    val json = JsonParser.parseString(newMessage)
-                    println("=================")
-                    println(json)
-                    println("=================")
-                }
-                foundOBR = true
-                oneLab = it + "\n"
-            } else
-                oneLab += it + "\n"
-        }
-        //process last OBR:
-        val newMessage = bumblebee.transformMessage(oneLab)
-        val json = JsonParser.parseString(newMessage)
-        println("=================")
-        println(json)
-        println("=================")
-
-    }
-//        obrs.get().iterator().forEach {obrsInnerArray ->
-//            obrsInnerArray.iterator().forEach {
 //
-//                val obr_4 = HL7StaticParser.getFirstValue(it, "OBR[1]-4.1")
-//                println(obr_4)
-//                if (EPI_OBR != obr_4.get() ) { //DO not convert EPI OBRS into Lab Orders
-//                    val obxs = HL7StaticParser.getValue(message, "OBR[@4.1='${obr_4.get()}']->OBX")
-//                    val spm = HL7StaticParser.getValue(message, "OBR[@4.1='${obr_4.get()}']->SPM")
+//        val lines =message.split("\n")
 //
-//                    var oneLab = it + "\n"
-//                    if (obxs.isDefined) {
-//                        obxs.get().iterator().forEach { obxInnerArray ->
-//                            obxInnerArray.iterator().forEach { obx ->
-//                                oneLab += obx + "\n"
-//                            }
-//                        }
-//                    }
-//                    if (spm.isDefined) {
-//                        oneLab += spm.get()
-//                    }
-//
+//        var oneLab = ""
+//        var foundOBR = false
+//        lines.forEach {
+//            if (it.startsWith("OBR|")) { //New Lab?
+//                //have we collected a lab yet?
+//                if (foundOBR) {
 //                    val newMessage = bumblebee.transformMessage(oneLab)
-//
 //                    val json = JsonParser.parseString(newMessage)
 //                    println("=================")
 //                    println(json)
 //                    println("=================")
 //                }
-
-//            }
+//                foundOBR = true
+//                oneLab = it + "\n"
+//            } else
+//                oneLab += it + "\n"
 //        }
-//        val newMessage = bumblebee.transformMessage(message)
-//
+//        //process last OBR:
+//        val newMessage = bumblebee.transformMessage(oneLab)
 //        val json = JsonParser.parseString(newMessage)
+//        println("=================")
 //        println(json)
-//    }
+//        println("=================")
+
+    //}
+ /*       nonEpiOBRs.get().iterator().forEach {obrsInnerArray ->
+            obrsInnerArray.iterator().forEach {
+
+                val obr_4 = HL7StaticParser.getFirstValue(it, "OBR[1]-4.1")
+                println(obr_4.get())
+                if (obr_4.get() !in listOf(EPI_OBR, "NOTF", "PERSUBJ") ) { //DO not convert EPI OBRS into Lab Orders
+                    val obxs = hl7Parser.getValue("OBR[@4.1='${obr_4.get()}']->OBX")
+                    val spm = hl7Parser.getValue( "OBR[@4.1='${obr_4.get()}']->SPM")
+
+                    var oneLab = it + "\n"
+                    if (obxs.isDefined) {
+                        obxs.get().iterator().forEach { obxInnerArray ->
+                            obxInnerArray.iterator().forEach { obx ->
+                                oneLab += obx + "\n"
+                            }
+                        }
+                    }
+                    if (spm.isDefined) {
+                        oneLab += spm.get()
+                    }
+
+                    val newMessage = bumblebee.transformMessage(oneLab)
+
+                    val json = JsonParser.parseString(newMessage)
+                    println("=================")
+                    println(json)
+                    println("=================")
+                } else {
+                    println("EPI OBR is in the list")
+                }
+
+            }
+        }
+*/
+    }
 }
