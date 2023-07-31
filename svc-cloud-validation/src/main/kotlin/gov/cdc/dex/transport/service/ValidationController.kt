77f8ac2198766d3d0bdcccb9ad23b9a3e55cfa6d
Win Swarr <USY6@CDC.gov>
2023-07-31 08:08:01 -0600

    [HL7-983] Adding test code for http client attempts

--- svc-cloud-validation/src/main/kotlin/gov/cdc/dex/transport/service/ValidationController.kt
+++ svc-cloud-validation/src/main/kotlin/gov/cdc/dex/transport/service/ValidationController.kt
@@ -1,29 +1,26 @@
 package gov.cdc.dex.validation.service
 
+//import gov.cdc.dex.hl7.Helper
+//import cdc.xlr.structurevalidator._
+
+import com.google.gson.*
 import io.micronaut.http.HttpRequest
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.MediaType
+import io.micronaut.http.MutableHttpRequest
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
-import io.micronaut.http.annotation.Delete
-import io.micronaut.http.annotation.Get
-import io.micronaut.http.annotation.PathVariable
 import io.micronaut.http.annotation.Post
-import io.micronaut.http.annotation.QueryValue
-import io.micronaut.http.multipart.CompletedFileUpload
 import org.slf4j.LoggerFactory
-import java.util.*
-//import gov.cdc.dex.hl7.Helper
-//import cdc.xlr.structurevalidator._
+import java.io.BufferedReader
+import java.io.DataOutputStream
 import java.io.InputStreamReader
+import java.io.OutputStream
 import java.net.HttpURLConnection
 import java.net.URL
-import java.io.BufferedReader
-import java.io.OutputStream
-
-import com.google.gson.*
-
-
+import java.util.*
+import java.io.*
+import java.net.*
 
 /**
  *
@@ -38,7 +35,6 @@ class ValidationController() {
 
     val redactorUrl = System.getenv("REDACTOR_URL") + "/api/redactorReport"
     val structureUrl = System.getenv("STRUCTURE_URL") + "/api/structure"
-    val elrType = System.getenv("ELR_TYPE")
 
     @Post(value = "/", consumes = [MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN])
     fun uploadContentDefault(
@@ -66,13 +62,50 @@ class ValidationController() {
     }
 
     private fun getContent(url: URL, payLoad : String, metadata: Map<String, String>? = null): String {
+
+        val byteArray : ByteArray
+        var resultData = ""
+        val conn = url.openConnection() as HttpURLConnection
+        conn.disconnect()
+        conn.requestMethod = "POST"
+        conn.setRequestProperty("x-tp-message_type", metadata?.get("x-tp-message_type"))
+        conn.setRequestProperty("x-tp-route", metadata?.get("x-tp-route"))
+        conn.useCaches = false
+        conn.doOutput = true
+        conn.connect()
+        //DataOutputStream(conn.outputStream).use { wr -> wr.write(byteArray) }
+        //val inputStream = conn.inputStream
+        DataOutputStream(conn.getOutputStream()).use { it.writeBytes(payLoad) }
+        BufferedReader(InputStreamReader(conn.getInputStream())).use { bf ->
+            var line: String?
+            while (bf.readLine().also { line = it } != null) {
+                println(line)
+            }
+        }
+
+        return resultData
+    }
+
+
+    private fun getContent_01(url: URL, payLoad : String, metadata: Map<String, String>? = null): String {
+        //"https://ocio-ede-dev-hl7-structure-validator.azurewebsites.net/api/structure"
+
+        val response = HttpRequest.POST(url.toString(), payLoad).headers.add("x-tp-message_type", "CASE")
+        val content = (response as MutableHttpRequest<*>).contentType(MediaType.TEXT_PLAIN_TYPE)
+        val result = response.body
+        val responseData = content.toString()
+        return result.toString()
+        //val flowable = httpClient.retrieve(req)
+        //return req as CompletableFuture<String>
+    }
+    private fun getContent_orig(url: URL, payLoad : String, metadata: Map<String, String>? = null): String {
         val sb = StringBuilder()
         val conn = url.openConnection() as HttpURLConnection
-        conn.requestMethod = "POST"
+        conn.requestMethod = "GET" //"POST"
         conn.doOutput = true
         conn.setRequestProperty("Accept", "application/json")
         conn.setRequestProperty("x-tp-message_type", metadata?.get("message_type"))
-        conn.setRequestProperty("x-tp-route", metadata?.get("route"))
+        conn.setRequestProperty("x-tp-route", metadata?.get("x-tp-route"))
         var os: OutputStream
         try {
             val os = conn.getOutputStream()
