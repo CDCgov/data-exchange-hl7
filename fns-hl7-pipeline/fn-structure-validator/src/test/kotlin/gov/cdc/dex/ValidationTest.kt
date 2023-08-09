534e9fc04dfc956a2b26c065a46966aaed060f80
Marcia Schulman <quq6@cdc.gov>
2023-08-09 15:28:13 -0400

    update validateMessage

--- fns-hl7-pipeline/fn-structure-validator/src/test/kotlin/gov/cdc/dex/ValidationTest.kt
+++ fns-hl7-pipeline/fn-structure-validator/src/test/kotlin/gov/cdc/dex/ValidationTest.kt
@@ -21,7 +21,7 @@ class ValidationTest {
 
     private fun validateMessage(fileName: String): NistReport {
         val testMessage = this::class.java.getResource(fileName).readText()
-        val phinSpec = HL7StaticParser.getFirstValue(testMessage, "MSH-21[1].1").get()
+        val phinSpec = HL7StaticParser.getFirstValue(testMessage, "MSH-21[1].1").get().uppercase()
      //   val phinSpec =testMessage.split("\r")[0].split("|")[20].split("^")[0]
         val nistValidator = ProfileManager(ResourceFileFetcher(), "/$phinSpec")
 
@@ -68,7 +68,7 @@ class ValidationTest {
     fun testExtractPhinSpec() {
         val msh = "MSH|^~\\\\&|SendAppName^2.16.840.1.114222.TBD^ISO|Sending-Facility^2.16.840.1.114222.TBD^ISO|PHINCDS^2.16.840.1.114222.4.3.2.10^ISO|PHIN^2.16.840.1.114222^ISO|20140630120030.1234-0500||ORU^R01^ORU_R01|MESSAGE CONTROL ID|D|2.5.1|||||||||NOTF_ORU_v3.0^PHINProfileID^2.16.840.1.114222.4.10.3^ISO~Generic_MMG_V2.0^PHINMsgMapID^2.16.840.1.114222.4.10.4^ISO~Lyme_TBRD_MMG_V1.0^PHINMsgMapID^2.16.840.1.114222.4.10.4^ISO"
         //val profile = msh.split("|")[20].split("^")[0]
-        val profile = HL7StaticParser.getFirstValue(msh, "MSH-21[1].1").get()
+        val profile = HL7StaticParser.getFirstValue(msh, "MSH-21[1].1").get().uppercase()
         println(profile)
     }
 
