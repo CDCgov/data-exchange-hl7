aa783d14c091a906e5a2bab5a6e900127e234535
Aguilar Salazar <csb0@cdc.gov>
2023-08-11 16:58:29 -0500

    recover UT deleted files for lake segments

---
+++ fns-hl7-pipeline/fn-lake-segs-transformer/src/test/kotlin/LakeSegsFunctionTest.kt
@@ -0,0 +1,83 @@
+import com.microsoft.azure.functions.ExecutionContext
+import gov.cdc.dex.azure.EventHubMetadata
+import gov.cdc.dex.hl7.Function
+import org.junit.jupiter.api.Test
+import java.util.logging.Logger
+import com.google.gson.JsonObject
+import com.google.gson.JsonArray
+import org.junit.jupiter.api.Assertions
+
+public class LakeSegsFunctionTest {
+
+    private fun processFile(filename:String, isHappyPath:Boolean) {
+        println("Start processing $filename ")
+        val text = this:: class.java.getResource("/$filename").readText()
+        val messages = listOf(text)
+        val eventHubMDList = listOf(EventHubMetadata(1, 99, "", ""))
+        val function = Function()
+        val inputEvents : List<JsonObject> = function.eventHubProcessor(messages, eventHubMDList, getExecutionContext())
+        val inputEvent : JsonObject = inputEvents[0]
+
+        // Validate Metadata.processes has been added to the array of proccesses
+        val metadata: JsonObject? = inputEvent.get("metadata").asJsonObject
+        if(metadata != null){
+            val processes: JsonArray? = metadata.get("processes").asJsonArray
+            Assertions.assertTrue(processes != null)
+        }
+        val summaryObj : JsonObject? = inputEvent.get("summary").asJsonObject
+        println("the summary object: $summaryObj")
+        if (summaryObj != null){
+            if(isHappyPath){
+                // Validate Summary.current_status is successful
+                Assertions.assertEquals("LAKE-SEGMENTS-TRANSFORMED", summaryObj.get("current_status").asString)
+            }
+            else{
+                // Validate current_status is unsuccessful
+                Assertions.assertEquals("LAKE-SEGMENTS-ERROR", summaryObj.get("current_status").asString)
+            }
+        }
+
+        println("Finished processing $filename ")
+    }
+
+    // @Test
+    // fun processELR_HappyPath() {
+    //     processFile("ELR_message.txt", true)
+    //     assert (true)
+    // }
+
+    @Test
+    fun processELR_ExceptionPath() {
+        processFile("ELR_Exceptionmessage.txt", false)
+        assert (true)
+    }
+
+    // @Test
+    // fun processCASE_HappyPath() {
+    //     processFile("CASE_message.txt", true)
+    //     assert (true)
+    // }
+
+    // @Test
+    // fun processCASE_ExceptionPath() {
+    //     processFile("ELR_Exceptionmessage.txt", false)
+    //     assert (true)
+    // }
+
+    private fun getExecutionContext():
+            ExecutionContext {
+        return object :ExecutionContext {
+            override fun getLogger():Logger {
+                return Logger.getLogger(Function:: class.java.name)
+            }
+
+            override fun getInvocationId():String {
+                return "null"
+            }
+
+            override fun getFunctionName():String {
+                return "null"
+            }
+        }
+    }
+}
---
+++ fns-hl7-pipeline/fn-lake-segs-transformer/src/test/kotlin/LakeSegsTransTest.kt
@@ -0,0 +1,135 @@
+import org.junit.jupiter.api.Test
+
+import kotlin.test.assertEquals
+// import kotlin.test.assertFailsWith
+// import kotlin.test.assertFails
+
+import org.slf4j.LoggerFactory
+import gov.cdc.dex.hl7.TransformerSegments
+
+// No longer used
+// import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+// import com.google.gson.reflect.TypeToken
+
+import gov.cdc.hl7.HL7HierarchyParser 
+import gov.cdc.hl7.model.HL7Hierarchy
+
+
+class LakeSegsTransTest {
+
+    companion object {
+
+        val logger = LoggerFactory.getLogger(LakeSegsTransTest::class.java.simpleName)
+        
+        // private val gson = Gson()
+        private val gsonWithNullsOn = GsonBuilder().serializeNulls().create() //.setPrettyPrinting().create()
+    } // .companion 
+
+
+    @Test
+    fun testLoggerInfo() {
+
+        logger.info("testLoggerInfo...")
+    } // .testLoggerInfo
+
+
+    @Test
+    fun testReadLocalResources() {
+
+        logger.info("testReadLocalResources...")
+
+        // read the profile
+        val profileFilePath = "/BasicProfile.json"
+        val profile = this::class.java.getResource(profileFilePath).readText()
+
+        assertEquals(100, profile.substring(0, 100).length)
+
+
+        // read the message:
+        val messagePath = "/Genv2_2-0-1_TC01.hl7"
+        val message = this::class.java.getResource(messagePath).readText()
+
+        assertEquals(100, message.substring(0, 100).length)
+    } // .testReadLocalResources
+
+
+    @Test
+    fun testPrintHL7HierarchyTree() {
+
+        var i = 0
+
+        fun printTree(node: HL7Hierarchy, ident: String) {   
+            
+            println("$ident ($i) --> ${node.segment().substring(0,3)}")   
+            i++
+            node.children().foreach { 
+                printTree(it, ident + " ")
+            }
+        } // .printTree 
+
+        logger.info("testPrintHL7HierarchyTree...")
+        
+        val filePath = "/Hepatitis_V1_0_1_TM_TC02_HEP_B_ACUTE_MOD.hl7"
+        val testMsg = this::class.java.getResource(filePath).readText()
+
+        // read the profile
+        val profileFilePath = "/BasicProfile.json"
+        val profile = this::class.java.getResource(profileFilePath).readText()
+
+
+        val msgTree = HL7HierarchyParser.parseMessageHierarchyFromJson(testMsg, profile)
+        printTree(msgTree, "")
+
+    } // .testPrintHL7HierarchyTree
+
+
+
+    @Test
+    fun testTransformerSegments() {
+
+        logger.info("testTransformerSegments...")
+        
+        val filePath = "/Hepatitis_V1_0_1_TM_TC02_HEP_B_ACUTE_MOD.hl7"
+        val testMsg = this::class.java.getResource(filePath).readText()
+
+        // read the profile
+        val profileFilePath = "/BasicProfile.json"
+        val profile = this::class.java.getResource(profileFilePath).readText()
+
+
+        val lakeSegsModel = TransformerSegments().hl7ToSegments(testMsg, profile)
+
+        logger.info("lakeSegsModel: --> $lakeSegsModel")
+
+        assertEquals(12, lakeSegsModel.size)
+    } // .testTransformerSegments
+
+
+    @Test
+    fun testTransformerSegmentsToJson() {
+
+        logger.info("testTransformerSegments...")
+        
+        val filePath = "/Hepatitis_V1_0_1_TM_TC02_HEP_B_ACUTE_MOD.hl7"
+        val testMsg = this::class.java.getResource(filePath).readText()
+
+        // read the profile
+        val profileFilePath = "/BasicProfile.json"
+        val profile = this::class.java.getResource(profileFilePath).readText()
+
+
+        val lakeSegsModel = TransformerSegments().hl7ToSegments(testMsg, profile)
+
+        val lakeSegsModelJson = gsonWithNullsOn.toJson(lakeSegsModel)
+
+        logger.info("lakeSegsModelJson: --> $lakeSegsModelJson")
+
+        assertEquals(100, lakeSegsModelJson.substring(0, 100).length)
+    } // .testTransformerSegmentsToJson
+
+
+} // .MmgSLakeSegsTransTestqlTest
+
+
+
