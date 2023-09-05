3fcb1f3acffc1535072e508df96cd909232b7c71
eph0 <eph0@cdc.gov>
2023-09-04 20:24:54 -0400

    initial cosmos db

--- fns-hl7-pipeline/fn-hl7-json-lake/src/main/kotlin/gov/cdc/dex/hl7/Function.kt
+++ fns-hl7-pipeline/fn-hl7-json-lake/src/main/kotlin/gov/cdc/dex/hl7/Function.kt
@@ -2,11 +2,7 @@ package gov.cdc.dex.hl7
 
 import com.google.gson.*
 import com.microsoft.azure.functions.ExecutionContext
-import com.microsoft.azure.functions.annotation.BindingName
-import com.microsoft.azure.functions.annotation.EventHubTrigger
-import com.microsoft.azure.functions.annotation.FunctionName
 import gov.cdc.dex.azure.EventHubMetadata
-import gov.cdc.dex.azure.EventHubSender
 import gov.cdc.dex.metadata.Problem
 import gov.cdc.dex.metadata.SummaryInfo
 import gov.cdc.dex.util.DateHelper.toIsoString
@@ -16,9 +12,16 @@ import gov.cdc.dex.util.JsonHelper.toJsonElement
 import gov.cdc.hl7.bumblebee.HL7JsonTransformer
 import java.util.*
 import com.google.gson.JsonObject
+import com.microsoft.azure.functions.OutputBinding
+import com.microsoft.azure.functions.annotation.*
 import org.slf4j.LoggerFactory
 
 
+private data class OutData(
+        val ok: MutableList<String> = mutableListOf(),
+        val err: MutableList<String> = mutableListOf(),
+        val cosmo: MutableList<JsonObject> = mutableListOf()
+)
 /**
  * Azure function with event hub trigger for the HL7 JSON Lake transformer
  * Takes an HL7 message and converts it to an HL7 json lake based on the PhinGuidProfile
@@ -33,33 +36,40 @@ class Function {
         const val SUMMARY_STATUS_ERROR = "HL7-JSON-LAKE-ERROR"
         val gsonWithNullsOn: Gson = GsonBuilder().serializeNulls().create()
 
-        val fnConfig = FunctionConfig()
         private var logger = LoggerFactory.getLogger(Function::class.java.simpleName)
     } // .companion object
 
 
     @FunctionName("HL7_JSON_LAKE_TRANSFORMER")
     fun eventHubProcessor(
-        @EventHubTrigger(
-            name = "msg",
-            eventHubName = "%EventHubReceiveName%",
-            connection = "EventHubConnectionString",
-            consumerGroup = "%EventHubConsumerGroup%",)
-        messages: List<String?>,
-        @BindingName("SystemPropertiesArray")eventHubMD:List<EventHubMetadata>,
-        context: ExecutionContext): JsonObject {
+            @EventHubTrigger(
+                    name = "msg",
+                    eventHubName = "%EventHubReceiveName%",
+                    consumerGroup = "%EventHubConsumerGroup%",
+                    connection = "EventHubConnectionString")
+            messages: List<String>?,
+            @BindingName("SystemPropertiesArray")eventHubMD:List<EventHubMetadata>,
+            context:ExecutionContext,
+            @EventHubOutput(name="jsonlakeOk",
+                    eventHubName = "%EventHubSendOkName%",
+                    connection = "EventHubConnectionString")
+            jsonlakeOkOutput : OutputBinding<List<String>>,
+            @EventHubOutput(name="jsonlakeErr",
+                    eventHubName = "%EventHubSendErrsName%",
+                    connection = "EventHubConnectionString")
+            jsonlakeErrOutput: OutputBinding<List<String>>,
+            @CosmosDBOutput(name="cosmosdevpublic",
+                    connection = "CosmosDBConnectionString",
+                    containerName = "hl7-json-lake", createIfNotExists = true,
+                    partitionKey = "/message_uuid", databaseName = "hl7-events")
+            cosmosOutput: OutputBinding<List<JsonObject>> ): JsonObject {
 
-        //
-        // Process each Event Hub Message
-        // ----------------------------------------------
-        // message.forEach { singleMessage: String? ->
         logger.info("DEX::${context.functionName}")
-
-        return processAllMessages(messages, eventHubMD, context) // .message.forEach
-
-    } // .eventHubProcessor
-
-    private fun processAllMessages( messages: List<String?>, eventHubMD: List<EventHubMetadata>, context: ExecutionContext):JsonObject {
+        if ( messages == null) {
+            return JsonObject()
+        }
+        //return if ( messages != null)  processAllMessages(messages, eventHubMD, context)  else  JsonObject()
+        val outData = OutData()
         messages.forEachIndexed { messageIndex: Int, singleMessage: String? ->
             val startTime = Date().toIsoString()
             try {
@@ -68,28 +78,44 @@ class Function {
                 val filePath =JsonHelper.getValueFromJson("metadata.provenance.file_path", inputEvent).asString
                 val messageUUID = inputEvent["message_uuid"].asString
 
+                // set id messageUUID, if missing
+                if (!inputEvent.has("id")) {
+                    inputEvent.add("id", messageUUID.toJsonElement())
+                }
+
                 logger.info("DEX::Processing messageUUID:$messageUUID")
                 try {
                     val hl7message = JsonHelper.getValueFromJsonAndBase64Decode("content", inputEvent)
                     val bumblebee = HL7JsonTransformer.getTransformerWithResource(hl7message, FunctionConfig.PROFILE_FILE_PATH)
                     val fullHL7 = bumblebee.transformMessage()
-                    updateMetadataAndDeliver(startTime, metadata, PROCESS_STATUS_OK, fullHL7, eventHubMD[messageIndex],
-                        fnConfig.evHubSender, fnConfig.eventHubSendOkName, gsonWithNullsOn, inputEvent, null,
-                        listOf(FunctionConfig.PROFILE_FILE_PATH))
-                    context.logger.info("Processed OK for HL7 JSON Lake messageUUID: $messageUUID, filePath: $filePath, ehDestination: ${fnConfig.eventHubSendOkName}")
-                    
+                    updateMetadataAndDeliver(startTime, metadata, PROCESS_STATUS_OK,
+                            fullHL7, eventHubMD[messageIndex], gsonWithNullsOn,
+                            inputEvent, null,
+                            listOf(FunctionConfig.PROFILE_FILE_PATH),
+                            outData)
+                    context.logger.info("Processed OK for HL7 JSON Lake messageUUID: $messageUUID, filePath: $filePath")
                     if (messageIndex == messages.lastIndex){
+                        with(outData) {
+                            jsonlakeOkOutput.value = ok
+                            jsonlakeErrOutput.value = err
+                            cosmosOutput.value = cosmo
+                        }
                         return inputEvent
                     }
-
                 } catch (e: Exception) {
                     context.logger.severe("Exception: Unable to process Message messageUUID: $messageUUID, filePath: $filePath, due to exception: ${e.message}")
                     //publishing the message  to the eventhubSendErrsName topic using EventHub
-                    updateMetadataAndDeliver(startTime, metadata, PROCESS_STATUS_EXCEPTION, null, eventHubMD[messageIndex],
-                        fnConfig.evHubSender, fnConfig.eventHubSendErrsName, gsonWithNullsOn, inputEvent, e,
-                        listOf(FunctionConfig.PROFILE_FILE_PATH))
-
-                    context.logger.info("Processed ERROR for HL7 JSON Lake messageUUID: $messageUUID, filePath: $filePath, ehDestination: ${fnConfig.eventHubSendErrsName}")
+                    updateMetadataAndDeliver(startTime, metadata, PROCESS_STATUS_EXCEPTION,
+                            null, eventHubMD[messageIndex], gsonWithNullsOn,
+                            inputEvent, e,
+                            listOf(FunctionConfig.PROFILE_FILE_PATH),
+                            outData)
+                    context.logger.info("Processed ERROR for HL7 JSON Lake messageUUID: $messageUUID, filePath: $filePath")
+                    with(outData) {
+                        jsonlakeOkOutput.value = ok
+                        jsonlakeErrOutput.value = err
+                        cosmosOutput.value = cosmo
+                    }
                     return inputEvent
                 } // .catch
 
@@ -100,13 +126,16 @@ class Function {
                 return JsonObject()
 
             } // .catch
-
         }
         return JsonObject()
     }
 
-    private fun updateMetadataAndDeliver(startTime: String, metadata: JsonObject, status: String, report: JsonObject?, eventHubMD: EventHubMetadata,
-                                         evHubSender: EventHubSender, evTopicName: String, gsonWithNullsOn: Gson, inputEvent: JsonObject, exception: Exception?, config: List<String>) {
+    private fun updateMetadataAndDeliver(
+            startTime: String, metadata: JsonObject, status: String,
+            report: JsonObject?, eventHubMD: EventHubMetadata, gsonWithNullsOn: Gson,
+            inputEvent: JsonObject, exception: Exception?,
+            config: List<String>,
+            outData:OutData) {
 
         val processMD = HL7JSONLakeProcessMetadata(status=status, report=report, eventHubMD = eventHubMD, config)
         processMD.startProcessTime = startTime
@@ -118,16 +147,12 @@ class Function {
             val problem = Problem(HL7JSONLakeProcessMetadata.PROCESS_NAME, exception, false, 0, 0)
             val summary = SummaryInfo(SUMMARY_STATUS_ERROR, problem)
             inputEvent.add("summary", summary.toJsonElement())
+            outData.err.add(gsonWithNullsOn.toJson(inputEvent))
         } else {
             inputEvent.add("summary", (SummaryInfo(SUMMARY_STATUS_OK, null).toJsonElement()))
+            outData.ok.add(gsonWithNullsOn.toJson(inputEvent))
         }
-        // enable for model
-        val inputEventOut = gsonWithNullsOn.toJson(inputEvent)
-        evHubSender.send(
-            evHubTopicName = evTopicName,
-            message = inputEventOut
-        )
-
+        outData.cosmo.add((gsonWithNullsOn.toJsonTree(inputEvent) as JsonObject))
     }
 
 } // .Function
