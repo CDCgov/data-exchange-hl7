e36e67b89040c271156ff137cddea20403a490c8
Marcia Schulman <quq6@cdc.gov>
2023-09-29 10:57:28 -0400

    update ErrorInfo model

--- svc-hl7-validation/src/main/kotlin/gov/cdc/dex/validation/service/model/ErrorInfo.kt
+++ svc-hl7-validation/src/main/kotlin/gov/cdc/dex/validation/service/model/ErrorInfo.kt
@@ -4,10 +4,8 @@ import com.google.gson.annotations.SerializedName
 data class ErrorInfo (
     val line: Int = 0,
     val column: Int = 0,
-    val path : String? = "",
+    val path : String = "",
     val description : String,
     val category : String = "Runtime Error",
-    val classification : String = "Error",
-    val stackTrace:  List<String>? = null,
-    val metaData : Map<String, String>? = null
+    val classification : String = "Error"
 )
