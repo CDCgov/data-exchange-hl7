761aff7f4b9a62df596b6604a72b8911564ae7c4
john-qeh3 <qeh3@cdc.gov>
2023-10-16 12:23:40 -0400

    remove unused import statements

--- fns-sidecars/fn-sc-cosmosdb-sync/src/main/kotlin/gov/cdc/dataexchange/Function.kt
+++ fns-sidecars/fn-sc-cosmosdb-sync/src/main/kotlin/gov/cdc/dataexchange/Function.kt
@@ -1,18 +1,9 @@
 package gov.cdc.dataexchange
 
-import com.azure.cosmos.models.PartitionKey
-import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import com.google.gson.reflect.TypeToken
 import gov.cdc.dataexchange.client.CosmosDBClient
-import java.util.*
-import com.microsoft.azure.functions.*
 import com.microsoft.azure.functions.annotation.*
 import gov.cdc.dataexchange.client.CosmosDBClient.Companion.ConnectionFactory.closeConnectionShutdownHook
 import gov.cdc.dataexchange.services.RecordService
-import org.slf4j.LoggerFactory
-import reactor.core.publisher.Flux
-import java.lang.reflect.Type
 
 /**
  * Azure Function implementations.
