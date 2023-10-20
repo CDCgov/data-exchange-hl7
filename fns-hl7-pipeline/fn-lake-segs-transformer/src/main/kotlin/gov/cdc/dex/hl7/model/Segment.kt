3eef3744bd365091992828706bb426fc9ee6a825
john-qeh3 <qeh3@cdc.gov>
2023-10-20 08:44:57 -0400

    replace parent array items with segment id instead of whole segment, reverse order

--- fns-hl7-pipeline/fn-lake-segs-transformer/src/main/kotlin/gov/cdc/dex/hl7/model/Segment.kt
+++ fns-hl7-pipeline/fn-lake-segs-transformer/src/main/kotlin/gov/cdc/dex/hl7/model/Segment.kt
@@ -1,14 +1,14 @@
-package gov.cdc.dex.hl7.model 
+package gov.cdc.dex.hl7.model
 
 import com.google.gson.annotations.SerializedName
 
-data class Segment ( 
+data class Segment (
 
   @SerializedName("segment") val segment: String, 
   @SerializedName("segment_number") val segmentNumber: Int, 
   @SerializedName("parent_segments") val parentSegments: List<String>?,
 
-) // .Profile
-
-
-
+) { // .Profile
+  @SerializedName("segment_id")
+  var segmentId: String? = if (segment != "root") "${segment.substring(0, 3)}-$segmentNumber" else "root"
+}
