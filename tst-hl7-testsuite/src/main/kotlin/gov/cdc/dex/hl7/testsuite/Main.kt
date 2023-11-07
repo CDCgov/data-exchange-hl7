170844f102b65866ccf010bbcf91177432b3013f
Smith <ewl0@cdc.gov>
2023-11-07 13:29:33 -0500

    updated pom.xml:  - version 1.0.22-SNAPSHOT for lib-dex-commons to use cosmos DB library  - dependency for logger  - jackson-core dependency for JSON data processing  - updated jvmTarget to 17  updated main.kt:  - added messagesMetadata mapOf(<String, mutableMapOf<String,String>)    that holds metadata for each message(TO BE UPDATED)  - Added getCurrentDateTimeWithSeconds that returns current date time    including seconds  - updated dropMessagesToABlobStorage() function to include appended    date and time and setting metadata  - implemented queryCosmosDB() using the library from lib-dex-commons updated mainTest.kt:  - Added some placeholder test cases

--- tst-hl7-testsuite/src/main/kotlin/gov/cdc/dex/hl7/testsuite/Main.kt
+++ tst-hl7-testsuite/src/main/kotlin/gov/cdc/dex/hl7/testsuite/Main.kt
@@ -1,84 +1,110 @@
 package gov.cdc.dex.hl7.testsuite
 
-import com.azure.cosmos.ConsistencyLevel
-import com.azure.cosmos.CosmosClientBuilder
-import com.azure.cosmos.models.SqlParameter
-import com.azure.cosmos.models.SqlQuerySpec
-import com.azure.storage.blob.BlobClient
 import com.azure.storage.blob.BlobServiceClientBuilder
 import com.azure.storage.blob.BlobContainerClient
-import com.azure.storage.blob.models.BlobStorageException
-import com.azure.storage.blob.options.BlobUploadFromFileOptions
+import com.fasterxml.jackson.databind.JsonNode
+
+import gov.cdc.dex.azure.cosmos.CosmosClient
+import org.slf4j.Logger
 
 import org.slf4j.LoggerFactory
-import java.io.File
 
+import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 object Companion{
     val cosmosDBName:String = System.getenv("COSMOS_DB_NAME")
     val cosmosDBKey:String = System.getenv("COSMOS_DB_KEY")
-    val cosmosDBConnectionString:String = System.getenv("COSMOS_DB_CONNECTION_STRING")
     val cosmosDBContainer:String = System.getenv("COSMOS_DB_CONTAINER_NAME")
-
+    val cosmosDBEndpoint:String = System.getenv("COSMOS_DB_ENDPOINT")
+    val cosmosDBPartitionKey:String = System.getenv("COSMOS_DB_PARTITION_KEY")
     val blobConnectionString:String = System.getenv("BLOB_INGEST_CONNECTION_STRING")
     val blobContainerName:String = System.getenv("BLOB_CONTAINER_NAME")
 
-    val hl7MessagesPath = "src/main/resources"
+    val logger: Logger? = LoggerFactory.getLogger(Function::class.java.name)
+    const val PATH_TO_MESSAGES = "src/main/resources/messages"
+    private const val MESSAGE_TYPE: String = "message_type"
+    private const val ROUTE: String= "route"
+    private const val REPORTING_JURISDICTION= "reporting_jurisdiction"
+    private const val ORIGINAL_FILE_NAME:String = "original_file_name"
+    const val headerContentType = "text/plain"
+
+    val uploadedBlobs: MutableSet<String> = mutableSetOf()
+
+    val messagesMetadata = mapOf("case.txt" to mutableMapOf<String,String>(MESSAGE_TYPE to "CASE", ORIGINAL_FILE_NAME to "case.txt"),
+        "test.hl7" to mutableMapOf<String,String>(MESSAGE_TYPE to "ELR", REPORTING_JURISDICTION to "48", ORIGINAL_FILE_NAME to "lab.hl7"),
+        "Valid-PHLIP-ORU-DataType-DT.txt" to mutableMapOf<String,String>(MESSAGE_TYPE to "CASE", REPORTING_JURISDICTION to "48", ORIGINAL_FILE_NAME to "messages/case.txt"),
+        "Valid-PHLIP-ORU-DataType-DTM.txt" to mutableMapOf<String,String>(MESSAGE_TYPE to "CASE", REPORTING_JURISDICTION to "48", ORIGINAL_FILE_NAME to "messages/case.txt"),
+        "Valid-PHLIP-ORU-DataType-FT.txt" to mutableMapOf<String,String>(MESSAGE_TYPE to "CASE", REPORTING_JURISDICTION to "48", ORIGINAL_FILE_NAME to "messages/case.txt"),
+        "Valid-PHLIP-ORU-DataType-TX.txt" to mutableMapOf<String,String>(MESSAGE_TYPE to "CASE", REPORTING_JURISDICTION to "48", ORIGINAL_FILE_NAME to "messages/case.txt"),
+        "Valid-PHLIP-ORU-DataType-NM.txt" to mutableMapOf<String,String>(MESSAGE_TYPE to "CASE", REPORTING_JURISDICTION to "48", ORIGINAL_FILE_NAME to "messages/case.txt"),
+        "Valid-PHLIP-ORU-DataType-SN.txt" to mutableMapOf<String,String>(MESSAGE_TYPE to "CASE", REPORTING_JURISDICTION to "48", ORIGINAL_FILE_NAME to "messages/case.txt")
+        )
 
 
-    val logger = LoggerFactory.getLogger(Function::class.java.name)
 }
+fun getCurrentDateTimeWithSeconds(): String {
+    val currentDateTime = LocalDateTime.now()
+    val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+    return currentDateTime.format(formatter)
+}
+fun addPayloadToTestResources(payloadAsJson: String, originalFileName:String){
+    val testResourcesDirectory = "src/test/resources"
+    val jsonFileWithPayload = File("$testResourcesDirectory/$originalFileName.json")
+    jsonFileWithPayload.writeText(payloadAsJson)
 
-
-
-fun dropMessagesToBlobStorage() {
-    /*
-    uploads messages  to a blob storage container
-     */
+}
+fun dropMessagesToABlobStorage() {
     val blobServiceClient = BlobServiceClientBuilder()
         .connectionString(Companion.blobConnectionString)
         .buildClient()
 
     val containerClient:BlobContainerClient = blobServiceClient.getBlobContainerClient(Companion.blobContainerName)
 
-    val directory = File(Companion.hl7MessagesPath)
+    val directoryWithMessages = File(Companion.PATH_TO_MESSAGES)
 
-    if (directory.exists()) {
-        directory.listFiles()?.forEach {
-            val blobClient = containerClient.getBlobClient(it.name)
+    if (directoryWithMessages.exists()) {
+        directoryWithMessages.listFiles()?.forEach {
+            val blobNameWithTimeAppended = it.name + getCurrentDateTimeWithSeconds()
+            val blobClient = containerClient.getBlobClient(blobNameWithTimeAppended)
+
             blobClient.upload(File(it.absolutePath).inputStream(),it.length(),true)
+
+            blobClient.setMetadata(Companion.messagesMetadata[it.name])
+            Companion.uploadedBlobs.add(blobNameWithTimeAppended)
         }
     }else{
-        Companion.logger.info ("Check Resources directory.")
+        Companion.logger?.info("Check Messages under main/resources/messages.")
     }
 }
 
 
-fun queryCosmosDB(message_uuid:String){
-    /*
-    queries cosmos db and returns the payload
-    */
-    val cosmosClient =CosmosClientBuilder()
-        .endpoint(Companion.cosmosDBConnectionString)
-        .key(Companion.cosmosDBKey)
-        .consistencyLevel(ConsistencyLevel.SESSION)
-        .buildClient()
+fun queryCosmosDB(){
+    val cosmosDBClient by lazy {
+        CosmosClient(
+            Companion.cosmosDBName,
+            Companion.cosmosDBContainer,
+            Companion.cosmosDBEndpoint,
+            Companion.cosmosDBKey,
+            Companion.cosmosDBPartitionKey
+        )
+    }
 
-    val database = cosmosClient.getDatabase(Companion.cosmosDBName)
-    val container = database.getContainer(Companion.cosmosDBContainer)
+    for (blob in Companion.uploadedBlobs){
+        val queryCosmosDBToRetrievePayload = "SELECT * FROM c WHERE c.metadata.provenance.ext_original_file_name=$blob"
+        val queryItem = cosmosDBClient.sqlReadItems(queryCosmosDBToRetrievePayload,Map::class.java).blockLast()
+        println (queryItem)
+    }
 
 
 }
 
-
-fun comparePayloads(){
-    /*
-
-     */
-}
 fun main() {
-    print("main")
-    dropMessagesToBlobStorage()
+    println("main")
+    dropMessagesToABlobStorage()
+    //queryCosmosDB()
+
 
 
 
--- tst-hl7-testsuite/src/test/kotlin/gov/cdc/dex/hl7/testsuite/MainTest.kt
+++ tst-hl7-testsuite/src/test/kotlin/gov/cdc/dex/hl7/testsuite/MainTest.kt
@@ -1,15 +1,72 @@
 package gov.cdc.dex.hl7.testsuite
 
+import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import kotlin.test.assertEquals
 
 import org.junit.jupiter.api.Test
+import java.io.File
 
 class MainTest{
     /*
     TO BE IMPLEMENTED
      */
     @Test
-    fun testMSH(){
-        assertEquals(7,7)
+    fun verifyPayloadsExist(){
+
+    }
+    @Test
+    fun verifyMessageInfo(){
+
+    }
+    @Test
+    fun verifySummaryWithProblemAttribute(){
+    }
+
+    @Test
+    fun verifyRequiredFieldsPIDCase(){
+
+    }
+    @Test
+    fun verifyRequiredFieldsPIDElr(){
+
+    }
+    @Test
+    fun verifyOBX2HasRightDatatypeInOBX5Case(){
+
+    }
+    @Test
+    fun verifyOBX2HasRightDatatypeInOBX5Elr(){
+
+    }
+    @Test
+    fun requiredFieldsMSHCase(){
+        val pathToNewPayloadForCaseFile = "src/test/resources/new-payloads/case.json"
+        val newPayload = File(pathToNewPayloadForCaseFile)
+
+        val pathToVerifiedPayloadForCaseFile = "src/test/resources/verified-payloads/case.json"
+        val verifiedPayload = File(pathToVerifiedPayloadForCaseFile)
+
+        if (newPayload.exists() && verifiedPayload.exists()) {
+            val jsonMapper = jacksonObjectMapper()
+            val jsonNewPayload:JsonNode = jsonMapper.readTree(newPayload)
+            val jsonVerifiedPayload:JsonNode = jsonMapper.readTree(verifiedPayload)
+
+
+            val fieldsToCompare = setOf("file_separator","encoding_characters",
+                "sending_application","message_type","message_control_id",
+                "processing_id","version_id")
+
+            for (field in fieldsToCompare) {
+                val fieldFromNewPayload = jsonNewPayload[field]
+                val fieldFromVerifiedPayload = jsonVerifiedPayload[field]
+
+                assertEquals(fieldFromNewPayload, fieldFromVerifiedPayload , "Required fields for MSH $field should match!")
+
+            }
+
+
+        }
+
     }
 }
