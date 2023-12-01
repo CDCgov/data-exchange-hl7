c6e68bc7ce1fa0cbdc3b191c44dc41361ca00181
Marcia Schulman <quq6@cdc.gov>
2023-12-01 14:09:10 -0500

    fix config-configs metadata issue

--- fns-hl7-pipeline/fn-structure-validator/src/main/kotlin/gov/cdc/dex/hl7/model/StructureValidatorProcessMetadata.kt
+++ fns-hl7-pipeline/fn-structure-validator/src/main/kotlin/gov/cdc/dex/hl7/model/StructureValidatorProcessMetadata.kt
@@ -4,7 +4,7 @@ import gov.cdc.dex.azure.EventHubMetadata
 import gov.cdc.dex.metadata.ProcessMetadata
 import gov.cdc.nist.validator.NistReport
 
-data class StructureValidatorProcessMetadata (override val status: String, val report: NistReport?, @Transient val eventHubMD: EventHubMetadata, val config : List<String>): ProcessMetadata(
+data class StructureValidatorProcessMetadata (override val status: String, val report: NistReport?, @Transient val eventHubMD: EventHubMetadata, @Transient val config : List<String>): ProcessMetadata(
     VALIDATOR_PROCESS, VALIDATOR_VERSION, status, eventHubMD, config) {
     companion object  {
         const val VALIDATOR_PROCESS = "STRUCTURE-VALIDATOR"
