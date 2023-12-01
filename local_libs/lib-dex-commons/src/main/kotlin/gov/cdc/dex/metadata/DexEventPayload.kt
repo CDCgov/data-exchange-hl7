00ba4a85c1454dc921245820a6e300ad3ec229fe
Marcia Schulman <quq6@cdc.gov>
2023-12-01 13:33:34 -0500

    add upload metadata to DexEventPayload

--- local_libs/lib-dex-commons/src/main/kotlin/gov/cdc/dex/metadata/DexEventPayload.kt
+++ local_libs/lib-dex-commons/src/main/kotlin/gov/cdc/dex/metadata/DexEventPayload.kt
@@ -6,9 +6,13 @@ import java.util.*
 data class DexEventPayload (
     @SerializedName("id") val id: String = UUID.randomUUID().toString(),
     @SerializedName("message_uuid") val messageUUID: String = id,
+    @SerializedName("upload_id") val uploadID: String?,
+    @SerializedName("destination_id") val destinationID: String?,
+    @SerializedName("destination_event") val destinationEvent: String?,
     @SerializedName("message_info") val messageInfo: DexMessageInfo,
     val metadata: DexMetadata,
     @SerializedName("summary") val summary: SummaryInfo,
-    @SerializedName("metadata_version") val metadataVersion: String = "0.0.1",
+    @SerializedName("schema_version") val metadataVersion: String = "0.0.1",
+    @SerializedName("schema_name") val schemaName: String = "DEX HL7v2",
     val content: String
 )
