ab84eb5df6227e565b025d8b7fc3fd731a149a3c
Marcia Schulman <quq6@cdc.gov>
2024-01-26 10:42:48 -0500

    add schema version, schema name to recdeb report

--- fns-hl7-pipeline/fn-receiver-debatcher/src/main/kotlin/gov/cdc/dex/hl7/ReceiverEventReport.kt
+++ fns-hl7-pipeline/fn-receiver-debatcher/src/main/kotlin/gov/cdc/dex/hl7/ReceiverEventReport.kt
@@ -9,7 +9,10 @@ data class ReceiverEventReport(
     @SerializedName("message_batch") var messageBatch : String = "SINGLE",
     @SerializedName("number_of_messages") var totalMessageCount: Int = 0,
     @SerializedName("number_of_messages_not_propagated") var notPropogatedCount: Int = 0,
-    @SerializedName("error_messages") val errorMessages: MutableList<ReceiverEventError> = mutableListOf()
+    @SerializedName("error_messages") val errorMessages: MutableList<ReceiverEventError> = mutableListOf(),
+    @SerializedName("metadata") val metadata: MockedMetadata = MockedMetadata(),
+    @SerializedName("schema_version") val schemaVersion: String = "0.0.1",
+    @SerializedName("schema_name") val schemaName: String = "hl7_debatch_report"
 )
 
 data class ReceiverEventError (
@@ -17,3 +20,13 @@ data class ReceiverEventError (
     @SerializedName("message_uuid") val messageUUID: String?,
     @SerializedName("error_message") val error: String?
         )
+
+// "mocked" metadata simulates structure of ProcessMetadata with only the
+// elements needed for the Processing Status API.
+data class MockedMetadata (
+    @SerializedName("processes") val processes: List<MockedProcess> = listOf(MockedProcess())
+        )
+
+data class MockedProcess (
+    @SerializedName("process_name") val processName: String = "RECEIVER"
+)
