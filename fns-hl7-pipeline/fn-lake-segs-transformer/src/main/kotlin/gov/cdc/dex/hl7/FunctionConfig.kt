82b87d6fc654df033e39f4eb2d6d14be2158b67d
Marcia Schulman <quq6@cdc.gov>
2024-02-06 18:47:09 -0500

    update lake of segments transformer to use DedicatedEventHubSender

--- fns-hl7-pipeline/fn-lake-segs-transformer/src/main/kotlin/gov/cdc/dex/hl7/FunctionConfig.kt
+++ fns-hl7-pipeline/fn-lake-segs-transformer/src/main/kotlin/gov/cdc/dex/hl7/FunctionConfig.kt
@@ -1,18 +1,19 @@
 package gov.cdc.dex.hl7
 
+import gov.cdc.dex.azure.DedicatedEventHubSender
 import gov.cdc.dex.azure.EventHubSender
 import gov.cdc.dex.azure.RedisProxy
 import gov.cdc.dex.mmg.MmgUtil
 
 class FunctionConfig {
 
-    val evHubSender: EventHubSender
+    val evHubSender: DedicatedEventHubSender
     val evHubSendName: String = System.getenv("EventHubSendName")
 
     init {
          //Init Event Hub connections
         val evHubConnStr = System.getenv("EventHubConnectionString")
-        evHubSender = EventHubSender(evHubConnStr)
+        evHubSender = DedicatedEventHubSender(evHubConnStr, evHubSendName)
 
     }
 }
