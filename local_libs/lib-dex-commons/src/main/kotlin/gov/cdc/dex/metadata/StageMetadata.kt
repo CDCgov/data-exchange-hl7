e425a695952f19214cec5fe3e403ad0be0982162
Neelima Kandukuri <dtx0@cdc.gov>
2024-02-16 08:55:38 -0500

    Added Eventgrid properties

--- local_libs/lib-dex-commons/src/main/kotlin/gov/cdc/dex/metadata/StageMetadata.kt
+++ local_libs/lib-dex-commons/src/main/kotlin/gov/cdc/dex/metadata/StageMetadata.kt
@@ -5,14 +5,22 @@ import gov.cdc.dex.azure.EventHubMetadata
 import gov.cdc.dex.util.DateHelper.toIsoString
 import java.util.*
 
+data class EventGridMetaData(
+    val topic: String,
+    val eventTime:String
+)
 abstract class StageMetadata(
     @SerializedName("process_name") val processName: String,
     @SerializedName("process_version") val processVersion: String,
-    @SerializedName("eventhub_queued_time") val eventhubQueuedTime: String,
-    @SerializedName("eventhub_offset") val eventHubOffSet: Long,
-    @SerializedName("eventhub_sequence_number") val eventHubSeqNbr: Int,
+    //@SerializedName("eventhub_queued_time") val eventhubQueuedTime: String,
+    val eventhub_queued_time: String? = " ",
+    val eventhub_offset:Long? = null,
+    val eventhub_sequence_number:Int? = null,
+    //@SerializedName("eventhub_offset") val eventHubOffSet: Long,
+    //@SerializedName("eventhub_sequence_number") val eventHubSeqNbr: Int,
     @Transient open val status: String?,
-    @SerializedName("configs") val configs: List<String>
+    @SerializedName("configs") val configs: List<String>,
+    val eventGrid_eventTime: String? = null
 ) {
     @SerializedName("start_processing_time")
     var startProcessTime: String = Date().toIsoString()
@@ -20,6 +28,8 @@ abstract class StageMetadata(
     var endProcessTime: String? = null
 
     constructor(processName:String, processVersion:String, status: String?, eventHubMetadata: EventHubMetadata, configs: List<String> ):
-            this(processName, processVersion, eventHubMetadata.EnqueuedTimeUtc, eventHubMetadata.Offset, eventHubMetadata.SequenceNumber, status, configs)
+            this(processName, processVersion, eventHubMetadata.EnqueuedTimeUtc, eventHubMetadata.Offset, eventHubMetadata.SequenceNumber, status, configs,null)
+    constructor(processName:String, processVersion:String, status: String?, eventGridMetadata: EventGridMetaData, configs: List<String> ):
+            this(processName, processVersion, null, null, null, status, configs,eventGridMetadata.eventTime)
 }
 
