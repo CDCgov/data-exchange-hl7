1f7a1f344e5d878b5f685320001105ed40e5bf41
Caldas <MCQ1@cdc.gov>
2024-02-23 15:31:30 -0500

    adding single/batch constant

--- local_libs/lib-dex-commons/src/main/kotlin/gov/cdc/dex/metadata/MessageMetadata.kt
+++ local_libs/lib-dex-commons/src/main/kotlin/gov/cdc/dex/metadata/MessageMetadata.kt
@@ -8,4 +8,9 @@ class MessageMetadata(
     @SerializedName("message_index") val messageIndex: Int =1,
     @SerializedName("message_hash") val messageHash: String,
 
-)
+) {
+    companion object {
+        const val SINGLE_FILE = "SINGLE"
+        const val BATCH_FILE = "BATCH"
+    }
+}
