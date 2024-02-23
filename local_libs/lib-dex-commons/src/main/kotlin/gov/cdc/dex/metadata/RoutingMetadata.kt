cb1421e39be9ab55d0e0e6637128f4f838208004
Caldas <MCQ1@cdc.gov>
2024-02-23 16:15:51 -0500

    all routing md strings

--- local_libs/lib-dex-commons/src/main/kotlin/gov/cdc/dex/metadata/RoutingMetadata.kt
+++ local_libs/lib-dex-commons/src/main/kotlin/gov/cdc/dex/metadata/RoutingMetadata.kt
@@ -5,7 +5,7 @@ import com.google.gson.annotations.SerializedName
 class RoutingMetadata (
     @SerializedName("ingested_file_path") val ingestedFilePath: String,
     @SerializedName("ingested_file_timestamp") val ingestedFileTimestamp: String,
-    @SerializedName("ingested_file_size") val ingestedFileSize: Int,
+    @SerializedName("ingested_file_size") val ingestedFileSize: String,
     @SerializedName("data_producer_id") val dataProducerId: String,
     @SerializedName("jurisdiction") val jurisdiction: String,
     @SerializedName("upload_id") val uploadId: String,
