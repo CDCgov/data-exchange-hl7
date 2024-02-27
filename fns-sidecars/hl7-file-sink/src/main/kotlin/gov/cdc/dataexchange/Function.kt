7fa8baeb94cf0c4432186a442dcad555f5ab81de
Marcia Schulman <quq6@cdc.gov>
2024-02-26 19:15:11 -0500

    begin changing for v2

--- fns-sidecars/hl7-file-sink/src/main/kotlin/gov/cdc/dataexchange/Function.kt
+++ fns-sidecars/hl7-file-sink/src/main/kotlin/gov/cdc/dataexchange/Function.kt
@@ -7,6 +7,7 @@ import gov.cdc.dex.util.JsonHelper
 import org.slf4j.LoggerFactory
 import java.text.SimpleDateFormat
 import java.util.*
+import kotlin.math.log
 
 class Function {
 
@@ -33,7 +34,7 @@ class Function {
             val inputEvent = try {
                 JsonParser.parseString(message) as JsonObject
             } catch (e: Exception) {
-                logger.error("ERROR: Unable to parse event message #$index as JSON. Aborting save.")
+                logger.error("ERROR: Unable to parse event message with index $index as JSON. Aborting save.")
                 null
             } ?: continue
             //remove content
@@ -41,44 +42,42 @@ class Function {
 
             val messageUUID = try {
                 // this is where the value will be in hl7 message json
-                JsonHelper.getValueFromJson("message_uuid", inputEvent).asString
+                JsonHelper.getValueFromJson("message_metadata.message_uuid", inputEvent).asString
             } catch (e: Exception) {
-                try {
-                    // this is where the value will be in recdeb-reports json
-                    JsonHelper.getValueFromJson("file_uuid", inputEvent).asString
-                } catch (e: Exception) {
-                    logger.error(
-                        "ERROR: Unable to locate message_uuid or file_uuid for event message #$index." +
-                                " Aborting save."
-                    )
-                    null
-                }
+                "ERROR: Unable to locate message_uuid or file_uuid for event message with index $index." +
+                        " Aborting save."
+                null
             } ?: continue
 
-            val originalDestId = getValueOrUnknown("routing_metadata.destination_id", inputEvent)
-            val uploadID = getValueOrUnknown("routing_metadata.upload_id", inputEvent)
-            val traceID = getValueOrUnknown("routing_metadata.trace_id", inputEvent)
-            val parentSpanID = getValueOrUnknown("routing_metadata.parent_span_id", inputEvent)
-
+            // get the metadata needed for routing
+            val routingMeta = inputEvent["routing_metadata"]
+            if (!(routingMeta == null || routingMeta.isJsonNull)) {
+                val routingMetadata = routingMeta.asJsonObject
+                val supportingMetadata = routingMetadata.remove("supporting_metadata")
+                val metaToAttach = mutableMapOf<String, String>()
+                routingMetadata.asMap().forEach { entry ->
+                    if (!entry.value.isJsonNull) {
+                        metaToAttach[entry.key] = entry.value.asString
+                    }
+                }
+                if (!supportingMetadata.isJsonNull) {
+                    supportingMetadata.asJsonObject.asMap().forEach { entry ->
+                        if (!entry.value.isJsonNull) {
+                            metaToAttach.putIfAbsent(entry.key, entry.value.asString)
+                        }
+                    }
+                }
+            } else {
+                logger.error("DEX::ERROR:Unable to locate routing_metadata")
+            }
             logger.info("DEX::Processing message $messageUUID")
             try {
-                // add metadata
-                val newMetadata = mutableMapOf<String, String>()
-                newMetadata["meta_destination_id"] = originalDestId
-
-                newMetadata["meta_ext_uploadid"] = uploadID
-                newMetadata["trace_id"] = traceID
-                newMetadata["parent_span_id"] = parentSpanID
-                // change event to match destination folder name
-                inputEvent.addProperty("meta_ext_event", fnConfig.blobStorageFolderName)
-                newMetadata["meta_ext_event"] = fnConfig.blobStorageFolderName
                 val folderStructure = SimpleDateFormat("YYYY/MM/dd").format(Date())
-
                 // save to storage container
                 this.saveBlobToContainer(
                     "${fnConfig.blobStorageFolderName}/$folderStructure/$messageUUID.txt",
                     gson.toJson(inputEvent),
-                    newMetadata
+                    metaToAttach
                 )
                 logger.info("DEX::Saved message $messageUUID.txt to sink ${fnConfig.blobStorageContainerName}/${fnConfig.blobStorageFolderName}")
             } catch (e: Exception) {
@@ -88,14 +87,6 @@ class Function {
             }
         }
 
-    }
-
-    private fun getValueOrUnknown(jsonPath: String, inputEvent: JsonObject): String {
-        return try {
-            JsonHelper.getValueFromJson(jsonPath, inputEvent).asString
-        } catch (e: Exception) {
-            "UNKNOWN"
-        }
     }
 
     fun saveBlobToContainer(blobName: String, blobContent: String, newMetadata: MutableMap<String, String>) {
