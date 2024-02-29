2ccc9812a18206c6168727ceb5b158c346745673
Caldas <MCQ1@cdc.gov>
2024-02-29 13:26:25 -0500

    bash to run deploy locally

---
+++ fns-sidecars/hl7-file-sink/deployAll.sh
@@ -0,0 +1,17 @@
+
+declare -a arr=("recdeb" "redacted" "struct" "json-lake" "lake-seg")
+
+
+if ! [[ "$1" =~ ^(dev|tst|stg)$ ]]; then
+	echo "You must pass the targe environment as paramter: dev, tst or stg"
+	return
+fi
+
+for i in "${arr[@]}"
+do
+  echo "@@@"
+  echo "@@@ Deploying $i on $1 Environment"
+  echo "@@@"
+  mvn clean package -DskipTests -Paz-"$1" -DFUNC="$i"
+  mvn azure-functions:deploy -DFUNC="$i"
+done
