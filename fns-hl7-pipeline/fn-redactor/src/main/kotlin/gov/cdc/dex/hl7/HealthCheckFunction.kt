c13297fd2e2093be9a4c460b06608d9891cf22c3
Marcia Schulman <quq6@cdc.gov>
2024-04-08 12:51:30 -0400

    start fixing redactor health check

--- fns-hl7-pipeline/fn-redactor/src/main/kotlin/gov/cdc/dex/hl7/HealthCheckFunction.kt
+++ fns-hl7-pipeline/fn-redactor/src/main/kotlin/gov/cdc/dex/hl7/HealthCheckFunction.kt
@@ -2,9 +2,9 @@ package gov.cdc.dex.hl7
 
 import com.microsoft.azure.functions.*
 import com.microsoft.azure.functions.annotation.*
-import com.azure.messaging.eventhubs.*
 import com.google.gson.JsonObject
 import gov.cdc.dex.util.JsonHelper.toJsonElement
+import gov.cdc.dex.azure.health.*
 import java.time.LocalDate
 
 import java.util.*
@@ -22,24 +22,7 @@ class HealthCheckFunction {
         val startTime = LocalDate.now()
         val evHubSendName: String = System.getenv("EventHubSendName")
         val evHubConnStr = System.getenv("EventHubConnectionString")
-        val isHealthy  = checkEventHubHealth(evHubConnStr, evHubSendName)
-        var eventHubStatus = "DOWNGRADED"
-        var issues = "Service is down"
-        if (isHealthy) {
-            eventHubStatus = "UP"
-            issues = ""
-        }
-
-        val period = java.time.Period.between(LocalDate.now(), startTime)
-        val result = JsonObject()
-        val deps = JsonObject()
-
-        result.add("status", "UP".toJsonElement())
-        result.add("total_checks_duration", period.toString().toJsonElement())
-        deps.add("service", "EventHub".toJsonElement())
-        deps.add("status", eventHubStatus.toJsonElement())
-        deps.add("health_issues", issues.toJsonElement())
-        result.add("dependency_health_checks", deps)
+        val result = DependencyChecker().checkEventHub()
 
         return request
                 .createResponseBuilder(HttpStatus.OK)
@@ -49,26 +32,4 @@ class HealthCheckFunction {
     }
 
 
-    private fun checkEventHubHealth(connectionString: String, eventHubName: String): Boolean {
-        // Set up EventProcessor to receive events from the Event Hub
-        val eventProcessor = EventProcessorClientBuilder()
-            .connectionString(connectionString, eventHubName)
-            .buildEventProcessorClient()
-
-        // Connect to Event Hub and check if it's healthy
-        return try {
-            if (!eventProcessor.isRunning) {
-                // Start the EventProcessor to further test connectivity
-                eventProcessor.start()
-            }
-            // Event Hub is considered healthy if no exceptions are thrown
-            true
-        } catch (e: Exception) {
-            false
-        } finally {
-            if (eventProcessor.isRunning) {
-                eventProcessor.stop()
-            }
-        }
-    }
 }
