d79791b7404944748a062abc7352616116e5b787
Marcia Schulman <quq6@cdc.gov>
2024-06-04 17:51:52 -0400

    update file-sink deploy zip

--- fns-sidecars/hl7-file-sink/deployZip.sh
+++ fns-sidecars/hl7-file-sink/deployZip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#declare -a arr=("recdeb" "redacted" "struct" "json-lake" "lake-seg")
-declare -a arr=("lake-seg")
+declare -a arr=("recdeb" "redacted" "struct" "json-lake" "lake-seg")
+
 if ! [[ "$1" =~ ^(dev|tst|stg|prd)$ ]]; then
 	echo "You must pass the targe environment as paramter: dev, tst, stg or prd"
 	return
@@ -24,12 +24,13 @@ zip -r ../../../$base_name *
 cd ../../..
 
 echo "Deploying Zip..."
-
+export LANG=C.UTF-8
 for i in "${arr[@]}"
 do
   echo "    Deploying $i on $1 Environment"
   az functionapp deployment source config-zip -g $hl7RG -n $function_rootname-$i-$env --src $base_name
     ### Set FN_VERSION:
+
   fn_version=$(cat pom.xml |grep -oPm1 "(?<=<version>)[^<]+")
   az functionapp config appsettings set --name $function_rootname-$i-$env --resource-group $hl7RG --settings FN_VERSION=$fn_version
 done
