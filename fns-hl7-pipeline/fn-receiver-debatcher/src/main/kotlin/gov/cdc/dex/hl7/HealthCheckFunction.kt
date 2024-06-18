251eb4e951037d0cfe60a317426eed68b4a48a3e
Marcia Schulman <quq6@cdc.gov>
2024-06-18 16:56:31 -0400

    update lib-dex dependency checker to check storage queue; add dependency checks to receiver-debatcher

--- fns-hl7-pipeline/fn-receiver-debatcher/src/main/kotlin/gov/cdc/dex/hl7/HealthCheckFunction.kt
+++ fns-hl7-pipeline/fn-receiver-debatcher/src/main/kotlin/gov/cdc/dex/hl7/HealthCheckFunction.kt
@@ -2,7 +2,11 @@ package gov.cdc.dex.hl7
 
 import com.microsoft.azure.functions.*
 import com.microsoft.azure.functions.annotation.*
+import gov.cdc.dex.azure.health.DependencyChecker
+import gov.cdc.dex.azure.health.DependencyHealthData
+import gov.cdc.dex.azure.health.HealthCheckResult
 import java.util.*
+import kotlin.time.measureTime
 
 class HealthCheckFunction {
     @FunctionName("health")
@@ -14,10 +18,35 @@ class HealthCheckFunction {
         )
         request: HttpRequestMessage<Optional<String>>
     ): HttpResponseMessage {
+        val result = HealthCheckResult()
+        val evHubSendName: String = System.getenv("EventHubSendName")
+        val evHubReportsName: String =System.getenv("EventReportsHubName")
+        val evHubConnStr = System.getenv("EventHubConnectionString")
+        val blobIngestConn = System.getenv("BlobIngestConnectionString")
+        val blobContainer = System.getenv("BlobIngestContainerName")
+        val blobAttachments = System.getenv("attachmentBlobConnString")
+        val queueName = System.getenv("queueName")
+        val depChecker = DependencyChecker()
+        val time = measureTime {
+            addToResult(depChecker.checkEventHub(evHubConnStr, evHubSendName), result)
+            addToResult(depChecker.checkEventHub(evHubConnStr, evHubReportsName), result)
+            addToResult(depChecker.checkStorageAccount(blobIngestConn, blobContainer), result)
+            addToResult(depChecker.checkStorageAccount(blobAttachments), result)
+            addToResult(depChecker.checkStorageQueue(blobIngestConn, queueName), result)
+        }
+        result.totalChecksDuration = time.toComponents { hours, minutes, seconds, nanoseconds ->
+            "%02d:%02d:%02d.%03d".format(hours, minutes, seconds, nanoseconds / 1000000)
+        }
+
         return request
             .createResponseBuilder(HttpStatus.OK)
             .header("Content-Type", "application/json")
-            .body("UP")
-            .build();
+            .body(result)
+            .build()
+    }
+
+    private fun addToResult(dependencyData: DependencyHealthData, result: HealthCheckResult) {
+        result.dependencyHealthChecks.add(dependencyData)
+        result.status = if (dependencyData.status == "UP") "UP" else "DOWNGRADED"
     }
 }
