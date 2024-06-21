13baaf344ddcf8511231ad4cd6e9c541ee04c3ca
Marcia Schulman <quq6@cdc.gov>
2024-06-21 10:37:51 -0400

    add check of receive event hub

--- fns-hl7-pipeline/fn-structure-validator/src/main/kotlin/gov/cdc/dex/hl7/HealthCheckFunction.kt
+++ fns-hl7-pipeline/fn-structure-validator/src/main/kotlin/gov/cdc/dex/hl7/HealthCheckFunction.kt
@@ -3,6 +3,7 @@ package gov.cdc.dex.hl7
 import com.microsoft.azure.functions.*
 import com.microsoft.azure.functions.annotation.*
 import gov.cdc.dex.azure.health.DependencyChecker
+import gov.cdc.dex.azure.health.DependencyHealthData
 import gov.cdc.dex.azure.health.HealthCheckResult
 import java.util.*
 import kotlin.time.measureTime
@@ -18,10 +19,12 @@ class HealthCheckFunction {
             request: HttpRequestMessage<Optional<String>>
     ): HttpResponseMessage {
         val result = HealthCheckResult()
+        val evHubConnStr = System.getenv("EventHubConnectionString")
+        val evHubReceiveName = System.getenv("EventHubReceiveName")
+        val checker = DependencyChecker()
         val time = measureTime {
-            result.dependencyHealthChecks.add(DependencyChecker().checkEventHub(
-                ValidatorFunction.fnConfig.evHubSender
-            ))
+            addToResult(checker.checkEventHub(evHubConnStr, evHubReceiveName), result)
+            addToResult(checker.checkEventHub(ValidatorFunction.fnConfig.evHubSender), result)
         }
         result.totalChecksDuration = time.toComponents { hours, minutes, seconds, nanoseconds ->
             "%02d:%02d:%02d.%03d".format(hours, minutes, seconds, nanoseconds / 1000000)
@@ -31,5 +34,9 @@ class HealthCheckFunction {
                 .header("Content-Type", "application/json")
                 .body(result)
                 .build();
+    }
+    private fun addToResult(dependencyData: DependencyHealthData, result: HealthCheckResult) {
+        result.dependencyHealthChecks.add(dependencyData)
+        if (dependencyData.status != "UP") result.status = "DOWNGRADED"
     }
 }
