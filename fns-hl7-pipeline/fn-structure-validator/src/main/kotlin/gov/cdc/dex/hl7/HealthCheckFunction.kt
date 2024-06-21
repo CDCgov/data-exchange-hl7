2c8b908f172d6b09df17bf654af746ae73d04d74
Marcia Schulman <quq6@cdc.gov>
2024-06-21 10:25:39 -0400

    add event hub check to structure validator health check

--- fns-hl7-pipeline/fn-structure-validator/src/main/kotlin/gov/cdc/dex/hl7/HealthCheckFunction.kt
+++ fns-hl7-pipeline/fn-structure-validator/src/main/kotlin/gov/cdc/dex/hl7/HealthCheckFunction.kt
@@ -2,7 +2,10 @@ package gov.cdc.dex.hl7
 
 import com.microsoft.azure.functions.*
 import com.microsoft.azure.functions.annotation.*
+import gov.cdc.dex.azure.health.DependencyChecker
+import gov.cdc.dex.azure.health.HealthCheckResult
 import java.util.*
+import kotlin.time.measureTime
 
 class HealthCheckFunction {
     @FunctionName("health")
@@ -14,10 +17,19 @@ class HealthCheckFunction {
             )
             request: HttpRequestMessage<Optional<String>>
     ): HttpResponseMessage {
+        val result = HealthCheckResult()
+        val time = measureTime {
+            result.dependencyHealthChecks.add(DependencyChecker().checkEventHub(
+                ValidatorFunction.fnConfig.evHubSender
+            ))
+        }
+        result.totalChecksDuration = time.toComponents { hours, minutes, seconds, nanoseconds ->
+            "%02d:%02d:%02d.%03d".format(hours, minutes, seconds, nanoseconds / 1000000)
+        }
         return request
                 .createResponseBuilder(HttpStatus.OK)
                 .header("Content-Type", "application/json")
-                .body("UP")
+                .body(result)
                 .build();
     }
 }
